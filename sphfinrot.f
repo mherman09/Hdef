eb7f150a82250dac2ce144bb864eac0578c70837
Matthew Herman <matthew.w.herman@gmail.com>
2014-03-07 13:21:40 -0500

    Modify sphfinrot.f
    1. Add -u option to let user input values manually
    2. Add -d option to use default file names.

--- sphfinrot.f
+++ sphfinrot.f
@@ -12,11 +12,11 @@ C----
       REAL*8 olon,olat,ilon,ilat,plon,plat,angle
       REAL*8 ix,iy,iz,px,py,pz,ox,oy,oz
       CHARACTER*30 ifile,ofile
+      INTEGER user
 
-      call gcmdln(ifile,ofile)
+      call gcmdln(ifile,ofile,user)
 
-      if (ifile.eq.'none') then
-          print *,'To see options, use -h flag'
+      if (user.eq.1) then
           print *,'Enter lon_init lat_init pole_lon pole_lat angle:'
           read *,ilon,ilat,plon,plat,angle
           call rotate(olon,olat,ilon,ilat,plon,plat,angle)
@@ -42,23 +42,23 @@ C----------------------------------------------------------------------c
       REAL*8 olon,olat,ilon,ilat,plon,plat,angle
       REAL*8 ix,iy,iz,px,py,pz,ox,oy,oz
       REAL*8 pi,d2r,r2d
-      PARAMETER (pi=4.0d0*atan(1.0d0),d2r=pi/1.8d2,r2d=1.8d2/pi)
+      PARAMETER (pi=4.0d0*datan(1.0d0),d2r=pi/1.8d2,r2d=1.8d2/pi)
 
       call lonlat2xyz(ix,iy,iz,ilon,ilat)
       call lonlat2xyz(px,py,pz,plon,plat)
 
-      ox = px*(px*ix+py*iy+pz*iz)*(1-cos(angle*d2r))
-     1         + ix*cos(angle*d2r)
-     2         + (-pz*iy+py*iz)*sin(angle*d2r)
-      oy = py*(px*ix+py*iy+pz*iz)*(1-cos(angle*d2r))
-     1         + iy*cos(angle*d2r)
-     2         + ( pz*ix-px*iz)*sin(angle*d2r)
-      oz = pz*(px*ix+py*iy+pz*iz)*(1-cos(angle*d2r))
-     1         + iz*cos(angle*d2r)
-     2         + (-py*ix+px*iy)*sin(angle*d2r)
+      ox = px*(px*ix+py*iy+pz*iz)*(1.0d0-dcos(angle*d2r))
+     1         + ix*dcos(angle*d2r)
+     2         + (-pz*iy+py*iz)*dsin(angle*d2r)
+      oy = py*(px*ix+py*iy+pz*iz)*(1.0d0-dcos(angle*d2r))
+     1         + iy*dcos(angle*d2r)
+     2         + ( pz*ix-px*iz)*dsin(angle*d2r)
+      oz = pz*(px*ix+py*iy+pz*iz)*(1.0d0-dcos(angle*d2r))
+     1         + iz*dcos(angle*d2r)
+     2         + (-py*ix+px*iy)*dsin(angle*d2r)
 
-      olon = atan2(oy,ox)*r2d
-      olat = asin(oz)*r2d
+      olon = datan2(oy,ox)*r2d
+      olat = dasin(oz)*r2d
 
       RETURN
       END
@@ -69,28 +69,31 @@ C----------------------------------------------------------------------C
 
       IMPLICIT none
       REAL*8 pi,d2r
-      PARAMETER (pi=4.0d0*atan(1.0d0),d2r=pi/1.8d2)
+      PARAMETER (pi=4.0d0*datan(1.0d0),d2r=pi/1.8d2)
       REAL*8 lon,lat,x,y,z
 
-      x = cos(lon*d2r)*cos(lat*d2r)
-      y = sin(lon*d2r)*cos(lat*d2r)
-      z = sin(lat*d2r)
+      x = dcos(lon*d2r)*dcos(lat*d2r)
+      y = dsin(lon*d2r)*dcos(lat*d2r)
+      z = dsin(lat*d2r)
 
       RETURN
       END
 
 C----------------------------------------------------------------------C
 
-      SUBROUTINE gcmdln(ifile,ofile)
+      SUBROUTINE gcmdln(ifile,ofile,user)
 
       IMPLICIT NONE
       CHARACTER*30 ifile,ofile,tag
-      INTEGER i,narg
+      INTEGER i,narg,user
 
-      ifile = 'none'
+      user = 0
+      ifile = 'sphfinrot.in'
       ofile = 'sphfinrot.out'
 
       narg = iargc()
+      if (narg.eq.0) call usage()
+
       i = 0
  9998 i = i + 1
       if (i.gt.narg) goto 9999
@@ -101,6 +104,10 @@ C----------------------------------------------------------------------C
           elseif (tag(1:2).eq.'-o') then
               i = i + 1
               call getarg(i,ofile)
+          elseif (tag(1:2).eq.'-d') then
+              write(*,*) 'Running with default file names'
+          elseif (tag(1:2).eq.'-u') then
+              user = 1
           elseif (tag(1:2).eq.'-h'.or.tag(1:2).eq.'-?') then
               call usage()
           endif
@@ -116,29 +123,33 @@ C----------------------------------------------------------------------C
       IMPLICIT none
 
       write(*,*)
-     1 'Usage: sphfinrot -f [IFILE] -o [OFILE] -h/-?'
+     1 'Usage: sphfinrot -f [IFILE] -o [OFILE] -d -u -h/-?'
       write(*,*)
-     1 '  -f [IFILE] (Default none) name of input file'
-      write(*,*)
-     1 '                            if no file name given, fault ',
-     2                            'parameters prompted for manual input'
+     1 '  -f [IFILE] (Default sphfinrot.in) name of input file'
       write(*,*)
      1 '  -o [OFILE] (Default sphfinrot.out) name of output file'
+      write(*,*)
+     1 '  -d         Run with default file names'
       write (*,*)
-     1 '  -h/-?        help'
+     1 '  -u         Prompt user to enter information through standard'
+      write (*,*)
+     1 '                 input for single calculation'
+      write (*,*)
+     1 '  -h/-?      Online help (this screen)'
       write (*,*) ''
       write (*,*)
      1 '  sphfinrot makes a counter-clockwise finite rotation of a',
      2     ' point around a pole'
       write (*,*) ''
       write (*,*)
-     1 '    input file'
+     1 '    Input file'
       write (*,*)
      1 '        lon_init lat_init pole_lon pole_lat angle'
       write (*,*)
      1 '         :  :'
+      write(*,*) ''
       write (*,*)
-     1 '    output file'
+     1 '    Output file'
       write (*,*)
      1 '        lon_final lat_final'
       write (*,*)
