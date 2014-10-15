5b4706c547b8f0003026427b03d665c6b2056c1a
Matthew Herman <matthew.w.herman@gmail.com>
2014-10-15 16:24:51 -0400

    Modify lsqsubs.f
    1. Add constrained least squares subroutine.

--- lsqsubs.f
+++ lsqsubs.f
@@ -13,18 +13,16 @@ C   /Users/mherman/Research/lapack-3.5.0/
 C        liblapack.a, libmglib.a, librefblas.a
 C----
       IMPLICIT none
-      INTEGER NMAX,ORDMAX,nobs,ord
-      PARAMETER (NMAX=40000,ORDMAX=10)
-      REAL*8 xobs(NMAX),yobs(NMAX),coeff(ORDMAX)
-      INTEGER nrhs,lwmax
-      PARAMETER (nrhs=1,lwmax=100)
+      INTEGER OBSMAX,ORDMAX,nrhs,lwmax
+      PARAMETER (OBSMAX=100000,ORDMAX=20,nrhs=1,lwmax=100000)
+      REAL*8 xobs(OBSMAX),yobs(OBSMAX),coeff(ORDMAX)
       CHARACTER*1 trans
-      INTEGER m,n,lda,ldb,lwork,info,i,j
+      INTEGER nobs,ord,m,n,lda,ldb,lwork,info,i,j
       REAL*8  a(nobs,ord+1),b(nobs,nrhs),work(lwmax)
 
       trans = 'N' ! A matrix has form (nobs x nparam)
-      m = nobs    ! Number of rows in matrix
-      n = ord+1   ! Number of columns in matrix
+      m = nobs    ! Number of rows in matrix A
+      n = ord+1   ! Number of columns in matrix A
       lda = nobs  ! Leading dimension of A  lda >= max(1,m)
       ldb = nobs  ! Leading dimension of b  ldb >= max(1,m,n)
 
@@ -58,3 +56,75 @@ C----
       RETURN
       END
 
+C----------------------------------------------------------------------C
+
+      SUBROUTINE polylsqconstr(coeff,xobs,yobs,nobs,ord,xconstr,yconstr,
+     1                         nconstr)
+C---- 
+C Compute the best-fitting polynomial through a set of points
+C consisting of nobs ordered pairs (xobs(i),yobs(i)), constrained
+C to pass through nconstr points (xconstr(i),yconstr(i)). The
+C polynomial is of order "ord" (lin = 1, quad = 2, ...), and the
+C (ord+1) coefficients are returned in the variable "coeff."
+C
+C Solution to system of linear equations with equality constraints is
+C computed using LAPACK subroutine dgglse.
+C----
+      IMPLICIT none
+      INTEGER OBSMAX,ORDMAX,CONSTRMAX,lwmax
+      PARAMETER (OBSMAX=100000,ORDMAX=20,CONSTRMAX=21,lwmax=100000)
+      REAL*8 xobs(OBSMAX),yobs(OBSMAX),coeff(ORDMAX),
+     1       xconstr(CONSTRMAX),yconstr(CONSTRMAX)
+      INTEGER nobs,ord,nconstr,m,n,p,lda,ldb,lwork,info,i,j
+      REAL*8 a(nobs,ord+1),b(nconstr,ord+1),c(nobs),d(nconstr),
+     1       work(lwmax),x(ord+1)
+
+      if (nconstr.gt.ord+1) then
+          write(*,*) 'Error: nconstr > ord+1'
+          write(*,*) 'The number of equality constraints cannot be ',
+     1               'larger than the order of the polynomial plus one.'
+          STOP
+      endif
+
+      m = nobs      ! Number of rows in matrix A
+      n = ord+1     ! Number of columns in matrices A and B
+      p = nconstr   ! Number of rows in matrix B
+      lda = nobs    ! Leading dimension of A  lda >= max(1,m)
+      ldb = nconstr ! Leading dimension of N  ldb >= max(1,p)
+
+C---
+C Load model matrix (A), constraint matrix (B), observation vector (c),
+C and the constraint RHS vector (d).
+C---  
+      do 101 j = 1,ord+1
+          do 102 i = 1,nobs
+              a(i,j) = xobs(i)**(j-1)
+  102     continue
+          do 103 i = 1,nconstr
+              b(i,j) = xconstr(i)**(j-1)
+  103     continue
+  101 continue
+      do 104 i = 1,nobs
+          c(i) = yobs(i)
+  104 continue
+      do 105 i = 1,nconstr
+          d(i) = yconstr(i)
+  105 continue
+
+C----
+C Determine the optimal workspace size for the solution
+C----
+      lwork = -1
+      call dgglse(m,n,p,a,lda,b,ldb,c,d,x,work,lwork,info)
+      lwork = min(lwmax,int(work(1)))
+
+C----
+C Compute the polynomial coefficients
+C----
+      call dgglse(m,n,p,a,lda,b,ldb,c,d,x,work,lwork,info)
+      do 301 i = 1,ord+1
+          coeff(i) = x(i)
+  301 continue
+
+      RETURN
+      END
