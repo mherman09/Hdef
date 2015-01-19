785d672111085f8356824c5b7189074fd6cfbf23
Matthew Herman <matthew.w.herman@gmail.com>
2015-01-19 09:40:22 -0500

    Modify dateutil.f
    1. Compartmentalize code more, separating command
       line options, checking for inputs, and computing
       dates into separate subroutines.
    2. Add option to use different formats for inputs.
    3. Add option for long format input/output with
       hours, minutes, and seconds.
    4. Add subroutine rdarg to parse input format.
    5. Add option to input date/days on command line
       instead of through files.
    6. Update usage subroutine to reflect changes.

--- dateutil.f
+++ dateutil.f
@@ -4,129 +4,191 @@ C Utilities for converting to/from calendar dates and days
 C----
       IMPLICIT none
       INTEGER yr1,mo1,yr2,mo2
-      REAL*8 dy1,dy2,hr1,mn1,sc1,hr2,mn2,sc2,jd1,jd2,ndy,rem
-      CHARACTER*40 ifile,ofile,date1,date2,arg1,arg2
-      INTEGER opt,p,long
+      REAL*8 dy1,dy2,ndy
+      COMMON /DVARS/ yr1,mo1,yr2,mo2,dy1,dy2,ndy
+      REAL*8 rem
+      CHARACTER*40 ifile,ofile,date1,date2
+      CHARACTER*80 dum
+      CHARACTER*240 arg,ans
+      INTEGER opt,p,long,c,ptr,space,length,ntot,i,nline,ifmt
       LOGICAL ex
 
 C----
-C Parse command line and check arguments
+C Parse command line
 C----
-      call gcmdln(ifile,ofile,arg1,arg2,opt,long,p)
-      if (ifile.eq.'none'.and.arg1.eq.'undef') then
-          write(*,*) '!! Error: Input data is not specified'
-          call usage('!! Use -nday[f] or -date[f]')
-      elseif (arg1.eq.'undef') then
-          inquire(file=ifile,EXIST=ex)
-          if (.not.ex) call usage('!! Error: no input file: '//ifile)
-      endif
-      if (ofile.eq.'none'.and.p.eq.0) then
-          write(*,*) '!! Error: no output file specified'
-          call usage('!! Use -o OFILE')
-      endif
+      call gcmdln(ifile,ofile,opt,c,arg,long,p)
 
 C----
-C If dates/days defined on command line, compute and print results
+C Check that all necessary inputs/outputs defined
 C----
-      if (arg1.ne.'undef') then
-          call readdatec(arg1,yr1,mo1,dy1,long)
-          call date2jd(jd1,yr1,mo1,dy1)
-          ! DATE1 DATE2 -> NDAY
-          if (opt.eq.1) then
-              call readdatec(arg2,yr2,mo2,dy2,long)
-              call date2jd(jd2,yr2,mo2,dy2)
-              ndy = jd2 - jd1
-              write (*,9997) ndy
-          ! DATE1 NDAY -> DATE2
-          elseif (opt.eq.2) then
-              read(arg2,'(BN,F10.0)') ndy
-              jd2 = jd1 + ndy
-              call jd2date(yr2,mo2,dy2,jd2)
-              if (long.eq.1) then
-                  call dyhrmnsc(dy2,hr2,mn2,sc2)
-                  write(*,9999) yr2,mo2,dy2,hr2,mn2,sc2
-              else
-                  write(*,9998) yr2,mo2,dy2 
-              endif
-          endif
-      else
-          open (unit=11,file=ifile,status='old')
-          if (p.eq.0) open (unit=12,file=ofile,status='unknown')
-          if (opt.eq.1) then
-  111         if (long.eq.0) then
-                  read(11,*,end=113) yr1,mo1,dy1,yr2,mo2,dy2
-              else
-                  read(11,*,end=113) yr1,mo1,dy1,hr1,mn1,sc1,
-     1                               yr2,mo2,dy2,hr2,mn2,sc2
-                  dy1 = dy1 + hr1/2.4d1 + mn1/1.44d3 + sc1/8.64d4
-                  dy2 = dy2 + hr2/2.4d1 + mn2/1.44d3 + sc2/8.64d4
-              endif
-              call date2jd(jd1,yr1,mo1,dy1)
-              call date2jd(jd2,yr2,mo2,dy2)
-              ndy = jd2 - jd1
+      call chkopt(ifile,ofile,opt,p,c)
+
+C----
+C Calculate date/day
+C----
+      if (p.eq.0) open(unit=12,file=ofile,status='unknown')
+      if (c.eq.0) then
+          open(unit=11,file=ifile,status='old')
+  102     read(11,'(A240)',end=101) arg
+              call getfmt(arg,ifmt)
+              call rdarg(arg,ifmt,opt,long)
+              call getans(ans,opt,long)
               if (p.eq.0) then
-                  write (12,9997) ndy
+                  write(12,'(A80)') ans
               else
-                  write (*,9997) ndy
+                  write(*,'(A80)') ans
               endif
-              goto 111
-          else
-  112         if (long.eq.0) then
-                  read(11,*,end=113) yr1,mo1,dy1,ndy
-              else
-                  read(11,*,end=113) yr1,mo1,dy1,hr1,mn1,sc1,ndy
-                  dy1 = dy1 + hr1/2.4d1 + mn1/1.44d3 + sc1/8.64d4
-              endif
-              call date2jd(jd1,yr1,mo1,dy1)
-              jd2 = jd1 + ndy
-              call jd2date(yr2,mo2,dy2,jd2)
-              if (long.eq.1) then
-                  call dyhrmnsc(dy2,hr2,mn2,sc2)
-                  if (p.eq.0) then
-                      write(12,9999) yr2,mo2,dy2,hr2,mn2,sc2
-                  else
-                      write(*,9999) yr2,mo2,dy2,hr2,mn2,sc2
-                  endif
-              else
-                  if (p.eq.0) then
-                      write(12,9998) yr2,mo2,dy2
-                  else
-                      write(*,9998) yr2,mo2,dy2
-                  endif
-              endif
-              goto 112
-          endif
+              goto 102
+  101     continue
+      else
+          call getfmt(arg,ifmt)
+          call rdarg(arg,ifmt,opt,long)
+          call getans(ans,opt,long)
+          write(*,'(A80)') ans
       endif
-  113 continue
-
- 9997 format(F14.5)
- 9998 format(I6,I4,1F6.1)
- 9999 format(I6,I4,4F6.1)
-
+C
       END
 
 C======================================================================C
 
-      SUBROUTINE readdatec(datec,yr,mo,dy,long)
+      SUBROUTINE getfmt(arg,ifmt)
+C----
+C Format types:
+C   ifmt=1: YYYYMMDD[HHMMSS]
+C   ifmt=2: YYYY MM DD [HH MM SS]
+C   ifmt=3: YYYY-MM-DDT[HH:MM:SS]
+C---- 
       IMPLICIT none
-      CHARACTER*40 datec
-      INTEGER yr,mo,long
-      REAL*8 dy,hr,mn,sc
-      read(datec(1:4),'(BN,I4)') yr
-      read(datec(5:6),'(BN,I2)') mo
-      read(datec(7:8),'(BN,F10.0)') dy
-      if (long.eq.1) then
-          read(datec(9:10),'(BN,F10.0)') hr
-          read(datec(11:12),'(BN,F10.0)') mn
-          read(datec(13:14),'(BN,F10.0)') sc
-          dy = dy + hr/2.4d1 + mn/1.44d3 + sc/8.64d4
+      CHARACTER*240 arg
+      CHARACTER*40 dum
+      INTEGER ifmt,ptr
+      read(arg,*) dum ! Read arg into dum with "*" to ignore leading whitespace
+      ifmt = 1
+      ptr = index(dum,' ')
+      if (ptr.le.5) ifmt = 2
+      ptr = index(dum,'T')
+      if (ptr.gt.0) ifmt = 3
+      ptr = index(dum,'-')
+      if (ptr.gt.0) ifmt = 3
+      close(41)
+      RETURN
+      END
+
+C----------------------------------------------------------------------C
+
+      SUBROUTINE rdarg(arg,ifmt,opt,long)
+      IMPLICIT none
+      CHARACTER*240 ans,arg,arg1,arg2
+      INTEGER opt,long,ifmt
+      INTEGER yr1,mo1,yr2,mo2
+      REAL*8 dy1,dy2,ndy
+      REAL*8 hr1,mn1,sc1,hr2,mn2,sc2
+      COMMON /DVARS/ yr1,mo1,yr2,mo2,dy1,dy2,ndy
+      if (ifmt.eq.1) then
+          read(arg,*) arg1,arg2
+          read(arg1(1:4),*) yr1
+          read(arg1(5:6),*) mo1
+          read(arg1(7:8),*) dy1
+          if (long.eq.1) then
+              read(arg1(9:10),*) hr1
+              read(arg1(11:12),*) mn1
+              read(arg1(13:14),*) sc1
+          endif
+          if (opt.eq.1) then
+              read(arg2(1:4),*) yr2
+              read(arg2(5:6),*) mo2
+              read(arg2(7:8),*) dy2
+              if (long.eq.1) then
+                  read(arg2(9:10),*) hr2
+                  read(arg2(11:12),*) mn2
+                  read(arg2(13:14),*) sc2
+              endif
+          else
+              read(arg2,*) ndy
+          endif
+      elseif (ifmt.eq.2) then
+          if (long.eq.0.and.opt.eq.1) then
+              read(arg,*) yr1,mo1,dy1,yr2,mo2,dy2
+          elseif (long.eq.0.and.opt.eq.2) then
+              read(arg,*) yr1,mo1,dy1,ndy
+          elseif (long.eq.1.and.opt.eq.1) then
+              read(arg,*,end=2001) yr1,mo1,dy1,hr1,mn1,sc1,
+     1                             yr2,mo2,dy2,hr2,mn2,sc2
+          elseif (long.eq.1.and.opt.eq.2) then
+              read(arg,*,end=2001) yr1,mo1,dy1,hr1,mn1,sc1,ndy
+          endif
+      elseif (ifmt.eq.3) then
+          read(arg,*) arg1,arg2
+          read(arg1(1:4),*) yr1
+          read(arg1(6:7),*) mo1
+          read(arg1(9:10),*) dy1
+          if (long.eq.1) then
+              read(arg1(12:13),*) hr1
+              read(arg1(15:16),*) mn1
+              read(arg1(18:19),*) sc1
+          endif
+          if (opt.eq.1) then
+              read(arg2(1:4),*) yr2
+              read(arg2(6:7),*) mo2
+              read(arg2(9:10),*) dy2    
+              if (long.eq.1) then
+                  read(arg2(12:13),*) hr2
+                  read(arg2(15:16),*) mn2
+                  read(arg2(18:19),*) sc2
+              endif
+          else
+              read(arg2,*) ndy
+          endif
       endif
+      if (opt.eq.1.and.long.eq.1) then
+          dy1 = dy1 + hr1/2.4d1 + mn1/1.44d3 + sc1/8.64d4
+          dy2 = dy2 + hr2/2.4d1 + mn2/1.44d3 + sc2/8.64d4
+      endif
+      RETURN
+ 2001 write(*,*) '!! Error: read long format inputs incorrectly'
+      call usage('!! Check input formatting')
+      END
+
+C----------------------------------------------------------------------C
+
+      SUBROUTINE getans(ans,opt,long)
+C----
+C Compute date or number of days from input
+C----
+      IMPLICIT none
+      CHARACTER*240 ans
+      INTEGER opt,long
+      INTEGER yr1,mo1,yr2,mo2
+      REAL*8 dy1,dy2,ndy
+      COMMON /DVARS/ yr1,mo1,yr2,mo2,dy1,dy2,ndy
+      REAL*8 jd1,jd2,hr,mn,sc
+      call date2jd(jd1,yr1,mo1,dy1)
+      if (opt.eq.1) then
+          call date2jd(jd2,yr2,mo2,dy2)
+          ndy = jd2 - jd1
+          write(ans,9997) ndy
+      else
+          jd2 = jd1 + ndy
+          call jd2date(yr2,mo2,dy2,jd2)
+          if (long.eq.0) then
+              write(ans,9998) yr2,mo2,dy2
+          elseif (long.eq.1) then
+              call dyhrmnsc(dy2,hr,mn,sc)
+              write(ans,9999) yr2,mo2,dy2,hr,mn,sc
+          endif
+      endif
+ 9997 format(F14.5)
+ 9998 format(I6,I4,1F6.1)
+ 9999 format(I6,I4,4F6.1)
       RETURN
       END
 
 C----------------------------------------------------------------------C
 
       SUBROUTINE dyhrmnsc(dy,hr,mn,sc)
+C----
+C Convert decimal day to day, hour, minute, second
+C----
       IMPLICIT none
       REAL*8 rem,dy,hr,mn,sc
       rem = mod(dy,1.0d0)
@@ -151,7 +213,6 @@ C----
       IMPLICIT none
       INTEGER year,month,yrp,mop
       REAL*8 day,A,B,C,D,jd
-
       if (year.le.1582) print *,'WARNING: CALCUATION MAY NOT WORK ',
      1                          'CORRECTLY FOR YEARS BEFORE 1582!'
 
@@ -177,7 +238,6 @@ C----
       endif
       D = floor(30.6001d0*(dble(mop)+1.0d0))
       jd = B+C+D+day+1720994.5
-
       RETURN
       END
 
@@ -190,7 +250,6 @@ C----
       IMPLICIT none
       REAL*8 jd,F,I,A,B,C,D,E,G,yr,mo,day
       INTEGER year,month
-
       jd = jd + 0.5d0
       F = jd - floor(jd)
       I = jd - F
@@ -217,23 +276,48 @@ C----
       endif
       month = int(mo)
       year = int(yr)
-
       RETURN
       END
 
 C----------------------------------------------------------------------C
 
-      SUBROUTINE gcmdln(ifile,ofile,arg1,arg2,opt,long,p)
+      SUBROUTINE chkopt(ifile,ofile,opt,p,c)
+      IMPLICIT none
+      CHARACTER*40 ifile,ofile
+      INTEGER opt,p,c
+      LOGICAL ex
+      if (opt.eq.0) then
+          write(*,*) '!! Error: Type of date computation unspecified'
+          call usage('!! Use -nday or -date')
+      endif
+      if (ifile.eq.'none'.and.c.eq.0) then
+          write(*,*) '!! Error: Input data is unspecified'
+          call usage('!! Use -f IFILE or -c ARGS')
+      elseif (c.eq.0) then
+          inquire(file=ifile,EXIST=ex)
+          if (.not.ex) call usage('!! Error: no input file: '//ifile)
+      endif
+      if (ofile.eq.'none'.and.p.eq.0) then
+          write(*,*) '!! Error: no output specified'
+          call usage('!! Use -o OFILE or -p')
+      endif
+      RETURN
+      END
+
+C----------------------------------------------------------------------C
+C
+      SUBROUTINE gcmdln(ifile,ofile,opt,c,arg,long,p)
       IMPLICIT NONE
-      CHARACTER*40 ifile,ofile,tag,arg1,arg2
-      INTEGER i,narg,opt,p,long
+      CHARACTER*40 ifile,ofile,tag
+      CHARACTER*240 arg
+      INTEGER i,narg,ii,j,opt,p,c,long,space,cin(2,6),nc
       ifile = 'none'
       ofile = 'none'
-      arg1 = 'undef'
-      arg2 = 'undef'
       opt = 0
       long = 0
       p = 0
+      c = 0
+      arg = 'none'
       narg = iargc()
       if (narg.eq.0) then
           call usage('!! Error: no command line arguments specified')
@@ -242,43 +326,50 @@ C----------------------------------------------------------------------C
   201 i = i + 1
       if (i.gt.narg) goto 202
           call getarg(i,tag)
-          if (tag(1:6).eq.'-ndayf') then
+          if (tag(1:5).eq.'-nday') then
               opt = 1
-              i = i + 1
-              call getarg(i,ifile)
-          elseif (tag(1:5).eq.'-nday') then
-              opt = 1
-              i = i + 1
-              call getarg(i,arg1)
-              i = i + 1
-              call getarg(i,arg2)
-              p = 1
-          elseif (tag(1:6).eq.'-datef') then
-              opt = 2
-              i = i + 1
-              call getarg(i,ifile)
           elseif (tag(1:5).eq.'-date') then
               opt = 2
-              i = i + 1
-              call getarg(i,arg1)
-              i = i + 1
-              call getarg(i,arg2)
-              p = 1
           elseif (tag(1:5).eq.'-long') then
               long = 1
+          elseif (tag(1:2).eq.'-c') then
+              c = 1
+              p = 1
+              j = 0
+  231         j = j + 1
+              if (i+j.gt.narg) then
+                  goto 202
+              else
+                  call getarg(i+j,tag)
+                  if (tag(1:1).eq.'-'.and.
+     1                     (ichar(tag(2:2)).lt.48.or.
+     2                               ichar(tag(2:2)).gt.57)) then
+                      i = i + j - 1
+                      goto 201
+                  else
+                      if (j.eq.1) then
+                          arg = tag
+                      else
+                          arg = arg(1:20*(j-1))//tag
+                      endif
+                      goto 231
+                  endif
+              endif
+          elseif (tag(1:2).eq.'-f') then
+              i = i + 1
+              call getarg(i,ifile)
           elseif (tag(1:2).eq.'-o') then
               i = i + 1
               call getarg(i,ofile)
           elseif (tag(1:2).eq.'-p') then
               p = 1
           elseif (tag(1:2).eq.'-h') then
-              call usage('')
+              call usage(' ')
           else
               call usage('!! Error: No option '//tag)
           endif
           goto 201
   202 continue
-
       RETURN
       END
 
@@ -294,40 +385,44 @@ C----------------------------------------------------------------------C
           write(*,*)
       endif
       write(*,*)
-     1 'Usage: dateutil -nday DATE1 DATE2 | -ndayf DATEFILE | -date ',
-     2               'DATE NDAY | -datef DAYFILE'
-      write(*,*)
-     3 '                -o OUTFILE [-long] [-p] [-h]'
+     1 'Usage: dateutil -f IFILE -o OFILE -nday|-date ',
+     2               '[-c DATE1 DATE2|NDAY] [-long] [-p] [-h]'
       write(*,*)
       write(*,*)
-     1 '-nday DATE1 DATE2  Number of days between ',
-     2                          'dates (YYYYMMDD[HHMMSS])'
+     1 '-f IFILE             Input date file (format depends on -nday|',
+     1                                    '-date and -long)'
       write(*,*)
-     1 '-ndayf DATEFILE    Number of days between dates (YYYY MM DD ',
-     2                           '[HH MM SS]) in DATEFILE'
+     1 '-o OFILE             Output file (depends on -nday|-date and ',
+     2                                            '-long)'
       write(*,*)
-     1 '-date DATE NDAY    Calendar date from DATE (YYYYMMDD[HHMMSS]) ',
-     2                      'and # of days (NDAY)'
+     1 '-nday                Compute number of days (IFILE format: ',
+     2                                  'DATE1 DATE2)'
       write(*,*)
-     1 '-dated DAYFILE     Calendar date from DATE (YYYY MM DD [HH ',
-     2                         'MM SS]) and # of days (NDAY) in DAYFILE'
+     1 '-date                Compute calendar date (IFILE format: ',
+     2                                      'DATE NDAY)'
       write(*,*)
-     1 '-long              Read/write all dates as YYYYMMDDHHMMSS or ',
-     2                            'YYYY MM DD HH MM SS'
+     1 '-c DATE1 DATE2|NDAY  Input dates and days on command line ',
+     2                        '(overrides -f and -o)'
       write(*,*)
-     1 '-o OUTFILE         Output of date computation'
+     1 '-long                Read/write all dates as ',
+     2                      'YYYYMMDDHHMMSS instead of YYYYMMDD'
       write (*,*)
-     1 '-p                 Print dates or number of days to standard ',
-     2                                       'output (overrides -o)'
+     1 '-p                   Print results to standard output ',
+     2                                  '(overrides -o)'
       write (*,*)
-     1 '-h                 Online help'
+     1 '-h                   Online help (this message)'
       write(*,*)
       write(*,*)
-     1 '  NOTE: Dates on command line must be in YYYYMMDD[HHMMSS] ',
-     2               'format, with no spaces.'
+     1 '  dateutil recognizes three DATE formats:'
       write(*,*)
-     1 '        Dates in files are delimited by spaces YYYY MM DD ',
-     2                            '[HH MM SS]'
-      write (*,*)
+     1 '    YYYY MM HH [HH MM SS]'
+      write(*,*)
+     1 '    YYYYMMHH[HHMMSS]'
+      write(*,*)
+     1 '    YYYY-MM-HHT[HH:MM:SS]'
+      write(*,*)
+     1 '  NDAY format: real number (to allow fractions of a ',
+     2                                        'day)'
+      write(*,*)
       STOP
       END
