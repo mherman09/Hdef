56533c77c0dfa8b240a35315591645911628317f
Matthew Herman <matthew.w.herman@gmail.com>
2015-03-05 13:42:51 -0500

    Modify wraplos.f
    1. Change wraplos usage to align with other programs.
    2. Check for input/output files.

--- wraplos.f
+++ wraplos.f
@@ -4,15 +4,29 @@ c Convert a line of sight displacement to a phase difference given
 c the wavelength of the InSAR satellite.
 c----
       IMPLICIT NONE
-      REAL pi,d2r
-      PARAMETER (pi=4.0*atan(1.0),d2r=pi/180.0)
-
-      REAL stlo,stla,ulos,wvl,phase
-      
+      REAL*8 pi,d2r,tpi
+      PARAMETER (pi=4.0d0*datan(1.0d0),d2r=pi/180.0d0,tpi=2.0d0*pi)
+      REAL*8 stlo,stla,ulos,wvl,phase
       INTEGER narg,i
-      CHARACTER*30 ifile,ofile
+      CHARACTER*40 ifile,ofile
+      LOGICAL ex
 
       call gcmdln(wvl,ifile,ofile)
+      if (wvl.le.0.0d0) then
+          write(*,*) '!! Error: wavelength is undefined'
+          call usage('!! Use -w WVLNTH')
+      endif
+      if (ifile.eq.'none') then
+          write(*,*) '!! Error: Input LOS vector file unspecified'
+          call usage('!! Use -f IFILE to specify input file')
+      else
+          inquire(file=ifile,EXIST=ex)
+          if (.not.ex) call usage('!! Error: no input file: '//ifile)
+      endif
+      if (ofile.eq.'none') then
+          write(*,*) '!! Error: No output file specified'
+          call usage('!! Use -o OFILE to specify phase output file')
+      endif
 
       open (unit=10,file=ifile,status='old')
       open (unit=20,file=ofile,status='unknown')
@@ -21,11 +35,11 @@ c----
 c Convert line of sight displacement to phase difference from satellite
 c----
    13 read (10,*,end=14) stlo,stla,ulos
-          ulos = 100.0*ulos
-          phase = 2.0*(2.0*pi)*ulos/wvl
-          phase = mod(phase,2.0*pi)
-          if (phase.lt.-pi) phase = phase + 2.0*pi
-          if (phase.gt.pi) phase = phase - 2.0*pi
+          ulos = 100.0d0*ulos
+          phase = 2.0d0*tpi*ulos/wvl
+          phase = dmod(phase,tpi)
+          if (phase.lt.-pi) phase = phase + tpi
+          if (phase.gt.pi) phase = phase - tpi
           write (20,*) stlo,stla, phase
           goto 13
    14 continue
@@ -35,19 +49,15 @@ c----
 C======================================================================C
 
       SUBROUTINE gcmdln(wvl,ifile,ofile)
-
       IMPLICIT none
-      REAL wvl
-      CHARACTER*30 tag,ifile,ofile
+      REAL*8 wvl
+      CHARACTER*40 tag,ifile,ofile
       INTEGER i,narg
-
-      wvl = 23.6
-      ifile = 'nez2los.out'
-      ofile = 'wraplos.out'
-
+      wvl = -1.0d0
+      ifile = 'none'
+      ofile = 'none'
       narg = iargc()
-      if (narg.eq.0) call usage()
-
+      if (narg.eq.0) call usage(' ')
       i = 0
    11 i = i + 1
       if (i.gt.narg) goto 12
@@ -55,57 +65,44 @@ C======================================================================C
           if (tag(1:2).eq.'-w') then
               i = i + 1
               call getarg(i,tag)
-              read (tag,'(BN,F6.0)') wvl
+              read (tag,'(BN,F12.0)') wvl
           elseif (tag(1:2).eq.'-f') then
               i = i + 1
               call getarg(i,ifile)
           elseif (tag(1:2).eq.'-o') then
               i = i + 1
               call getarg(i,ofile)
-          elseif (tag(1:2).eq.'-h'.or.tag(1:2).eq.'-?') then
-              call usage()
+          elseif (tag(1:2).eq.'-h') then
+              call usage(' ')
           endif
           goto 11
    12 continue
-
       RETURN
       END
 
 C----------------------------------------------------------------------C
 
-      SUBROUTINE usage()
+      SUBROUTINE usage(str)
       IMPLICIT none
-
+      INTEGER lstr
+      CHARACTER str*(*)
+      if (str.ne.' ') then
+          lstr = len(str)
+          write(*,*) str(1:lstr)
+          write(*,*)
+      endif
       write(*,*)
-     1 'Usage: wraplos -w [WAVELENGTH] -f [IFILE] -o [OFILE] -h/-?'
+     1 'Usage: wraplos -w WVLNTH -f IFILE -o OFILE [-h]'
       write(*,*)
-     1 '  -w [WAVELENGTH]  wavelength of satellite ',
-     2                                      'signal (in cm)'
       write(*,*)
-     1 '  -f [IFILE] (Default nez2los.out) name of input file'
+     1 '-w WVLNTH  Wavelength of radar signal (cm)'
       write(*,*)
-     1 '  -o [OFILE] (Default wraplos.out) name of output file'
+     1 '-f IFILE   Input LOS displacement file (lo la uLOS)'
+      write(*,*)
+     1 '-o OFILE   Output phase file (lo la phase(rad))'
       write (*,*)
-     1 '  -h/-?        help'
-      write (*,*) ''
+     1 '-h         Online help (this message)'
       write (*,*)
-     1 '  wraplos converts displacements to phase changes, given an ',
-     2    'observation wavelength'
-      write (*,*) ''
-      write (*,*)
-     1 '    input file'
-      write (*,*)
-     1 '        stlo stla uLOS'
-      write (*,*)
-     1 '         :  :'
-      write (*,*)
-     1 '    output file'
-      write (*,*)
-     1 '        stlo stla phase_change'
-      write (*,*)
-     1 '         :  :'
-      write (*,*) ''
-
       STOP
       END
 
