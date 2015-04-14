bc1e0945b6e971f94520780bd9a8eec8d50a313e
Matthew Herman <matthew.w.herman@gmail.com>
2015-04-14 12:10:33 -0400

    Modify fltinv.f
    1. Add (linear) 3-D inversion option.
       (NOTE: 3-D damping and smoothing NOT implemented yet!)

--- fltinv.f
+++ fltinv.f
@@ -1,18 +1,20 @@
       PROGRAM MAIN
       IMPLICIT none
       CHARACTER*80 fltf,obsf,smoof
+      INTEGER dimen,ios
       LOGICAL ex
       REAL*8 smooth,damp
       INTEGER OBSMAX,PARMAX
       PARAMETER (OBSMAX=10000,PARMAX=100)
-      REAL*8 flt(PARMAX,8),obs(OBSMAX,6)
+      REAL*8 flt(PARMAX,7),obs(OBSMAX,6)
       REAL*8 A(OBSMAX,PARMAX),b(OBSMAX,1),x(OBSMAX,1)
       INTEGER i,j,nobs,nflt
 
 C----
-C Parse command line
+C Parse command line and check required inputs present
 C----
-      call gcmdln(fltf,obsf,smooth,smoof,damp)
+      call gcmdln(fltf,obsf,smooth,smoof,damp,dimen)
+C Fault geometry file
       if (fltf.eq.'none') then
           write(*,*) '!! Error: Input fault geometry file unspecified'
           call usage('!! Use -flt FLTFILE to specify input file')
@@ -20,6 +22,7 @@ C----
           inquire(file=fltf,EXIST=ex)
           if (.not.ex) call usage('!! Error: no input file: '//fltf)
       endif
+C Displacement observation file
       if (obsf.eq.'none') then
           write(*,*) '!! Error: Observation file unspecified'
           call usage('!! Use -obs OBSFILE to specify input file')
@@ -27,34 +30,52 @@ C----
           inquire(file=obsf,EXIST=ex)
           if (.not.ex) call usage('!! Error: no input file: '//obsf)
       endif
+C Problem dimension
+      if (dimen.eq.0) then
+          write(*,*) '!! Error: Problem dimension unspecified'
+          call usage('!! Use -2 for 2D, -3 for 3D')
+      endif
 
 C----
-C Count number of observations and input parameters, and read in data
-C NOTE TO ME: START WITH 2D INVERSION, THEN CHANGE TO FULLY 3D
+C Count number of observations, and read in data
 C----
       call linect(nobs,obsf)
       open(unit=21,file=obsf,status='old')
       do 101 i = 1,nobs
-          ! 2D: y(m) z(m) uy(m) uz(m)
-          ! 3D: x(m) y(m) z(m) ux(m) uy(m) uz(m)
-          read(21,*) (obs(i,j),j=1,4)
+          if (dimen.eq.2) then
+              ! 2D: y(m) z(m) uy(m) uz(m)
+              read(21,*) (obs(i,j),j=1,4)
+          elseif (dimen.eq.3) then
+              ! 3D: x(m) y(m) z(m) ux(m) uy(m) uz(m)
+              read(21,*) (obs(i,j),j=1,6)
+          endif
   101 continue
       close(21)
 
+C----
+C Count number of fault segments, and read in data
+C----
       call linect(nflt,fltf)
       open(unit=22,file=fltf,status='old')
       do 102 i = 1,nflt
-          ! 2D: y(m) z(m) dip(deg) wid(m) len(m)
-          ! 3D: x(m) y(m) z(m) str(deg) dip(deg) rak(deg) wid(m) len(m)
-          read(22,*) (flt(i,j),j=1,5)
+          if (dimen.eq.2) then
+              ! 2D: y(m) z(m) dip(deg) wid(m) len(m)
+              read(22,*) (flt(i,j),j=1,5)
+          elseif (dimen.eq.3) then
+              ! 3D: x(m) y(m) z(m) str(deg) dip(deg) wid(m) len(m)
+              read(22,*) (flt(i,j),j=1,7)
+          endif
   102 continue
       close(22)
 
 C----
-C Generate Green's functions and load A matrix and b vector (2D)
+C Generate Green's functions and load A matrix and b vector
 C----
-      call green2d(A,b,obs,flt,nobs,nflt)
-C      call green3d(A,obs,flt,nobs,nflt)
+      if (dimen.eq.2) then
+          call green2d(A,b,obs,flt,nobs,nflt)
+      elseif (dimen.eq.3) then
+          call green3d(A,b,obs,flt,nobs,nflt)
+      endif
 
 C----
 C Add damping
@@ -76,7 +97,44 @@ C----
   103 continue
       END
 
-C======================================================================C
+C----------------------------------------------------------------------C
+
+      SUBROUTINE damping(A,b,nobs,nflt,damp)
+C----
+C Minimize length of solution to Ax = b with weighting factor damp.
+C m=nobs n=nflt d=damp
+C         [ A11 A12 ... A1n ]
+C     A = [ A21 A22 ... A2n ]
+C         [  :           :  ]
+C         [ Am1 Am2 ... Amn ]
+C
+C         [ d*d  0  ...  0  ]
+C  Append [  0  d*d ...  0  ]
+C         [                 ]
+C         [  0   0  ... d*d ]
+C----
+      IMPLICIT none
+      REAL*8 damp
+      INTEGER OBSMAX,PARMAX
+      PARAMETER (OBSMAX=10000,PARMAX=100)
+      REAL*8 A(OBSMAX,PARMAX),b(OBSMAX,1)
+      INTEGER i,j,nobs,nflt
+      if (damp.le.0.0d0) return
+      do 101 i = 1,nflt
+          b(nobs+i,1) = 0.0d0
+          do 102 j = 1,nflt
+              if (i.eq.j) then
+                  A(nobs+i,j) = damp*damp*1.0d0
+              else
+                  A(nobs+i,j) = 0.0d0
+              endif
+  102     continue
+  101 continue
+      nobs = nobs + nflt
+      RETURN
+      END
+
+C----------------------------------------------------------------------C
 
       SUBROUTINE smoothing(A,b,nobs,nflt,smooth,smoof)
 C----
@@ -134,43 +192,6 @@ C Read smoothing element linking file and load into A
 
 C----------------------------------------------------------------------C
 
-      SUBROUTINE damping(A,b,nobs,nflt,damp)
-C----
-C Minimize length of solution to Ax = b with weighting factor damp.
-C m=nobs n=nflt d=damp
-C         [ A11 A12 ... A1n ]
-C     A = [ A21 A22 ... A2n ]
-C         [  :           :  ]
-C         [ Am1 Am2 ... Amn ]
-C
-C         [ d*d  0  ...  0  ]
-C  Append [  0  d*d ...  0  ]
-C         [                 ]
-C         [  0   0  ... d*d ]
-C----
-      IMPLICIT none
-      REAL*8 damp
-      INTEGER OBSMAX,PARMAX
-      PARAMETER (OBSMAX=10000,PARMAX=100)
-      REAL*8 A(OBSMAX,PARMAX),b(OBSMAX,1)
-      INTEGER i,j,nobs,nflt
-      if (damp.le.0.0d0) return
-      do 101 i = 1,nflt
-          b(nobs+i,1) = 0.0d0
-          do 102 j = 1,nflt
-              if (i.eq.j) then
-                  A(nobs+i,j) = damp*damp*1.0d0
-              else
-                  A(nobs+i,j) = 0.0d0
-              endif
-  102     continue
-  101 continue
-      nobs = nobs + nflt
-      RETURN
-      END
-
-C----------------------------------------------------------------------C
-
       SUBROUTINE green2d(A,b,obs,flt,nobs,nflt)
 C----
 C Create Green's functions for 2-D case and load model matrix
@@ -178,7 +199,7 @@ C----
       IMPLICIT none
       INTEGER OBSMAX,PARMAX
       PARAMETER (OBSMAX=10000,PARMAX=100)
-      REAL*8 flt(PARMAX,8),obs(OBSMAX,6),A(OBSMAX,PARMAX),b(OBSMAX,1)
+      REAL*8 flt(PARMAX,7),obs(OBSMAX,6),A(OBSMAX,PARMAX),b(OBSMAX,1)
       INTEGER i,j,nobs,nflt
       REAL*8 vp,vs,dens
       REAL*8 xflt,yflt,zflt,xobs,yobs,zobs,dx,dy
@@ -221,6 +242,79 @@ C In 2-D, each observation corresponds to two rows in A, b
       RETURN
       END
 
+C----------------------------------------------------------------------C
+
+      SUBROUTINE green3d(A,b,obs,flt,nobs,nflt)
+      IMPLICIT none
+      REAL*8 pi,d2r
+      PARAMETER (pi=4.0d0*atan(1.0d0),d2r=pi/1.8d2)
+      INTEGER OBSMAX,PARMAX
+      PARAMETER (OBSMAX=10000,PARMAX=100)
+      REAL*8 flt(PARMAX,7),obs(OBSMAX,6),A(OBSMAX,PARMAX),b(OBSMAX,1)
+      INTEGER i,j,jj,nobs,nflt
+      REAL*8 vp,vs,dens
+      REAL*8 xflt,yflt,zflt,xobs,yobs,zobs,dx,dy
+      REAL*8 wid,len,dip,rak,slip,str
+      REAL*8 gfx,gfy,gfz
+      REAL*8 ddip,dstr,t,cost,sint
+C Poisson ratio = 0.25
+      vp = 7.0d3
+      vs = vp/dsqrt(3.0d0)
+      dens = 3.0d3
+C Load model matrix, A
+      slip = 1.0d0
+      do 151 j = 1,nflt
+          xflt = flt(j,1)
+          yflt = flt(j,2)
+          zflt = flt(j,3)
+          str = flt(j,4)
+          dip = flt(j,5)
+          wid = flt(j,6)
+          len = flt(j,7)
+C         variables for rotating to fault reference frame
+          t = d2r*(90.0d0-str)
+          cost = dcos(t)
+          sint = dsin(t)
+          do 152 i = 1,nobs
+              xobs = obs(i,1)
+              yobs = obs(i,2)
+              zobs = obs(i,3)
+              dx = xobs - xflt
+              dy = yobs - yflt
+C             rotate to along-strike, horizontal up-dip direction
+              dstr =  dx*cost + dy*sint
+              ddip = -dx*sint + dy*cost
+C             need Green's functions for strike- and dip-slip components
+              do 153 jj = 1,2
+                  if (jj.eq.1) then
+                      rak = 90.0d0
+                      call o92rect(gfx,gfy,gfz,dstr,ddip,zobs,zflt,dip,
+     1                             rak,wid,len,slip,vp,vs,dens)
+                      A(i,j) = gfx*cost - gfy*sint
+                      A(i+nobs,j) = gfx*sint + gfy*cost
+                      A(i+2*nobs,j) = gfz
+                  else
+                      rak = 0.0d0
+                      call o92rect(gfx,gfy,gfz,dstr,ddip,zobs,zflt,dip,
+     1                             rak,wid,len,slip,vp,vs,dens)
+                      A(i,j+nflt) = gfx*cost - gfy*sint
+                      A(i+nobs,j+nflt) = gfx*sint + gfy*cost
+                      A(i+2*nobs,j+nflt) = gfz
+                  endif
+  153         continue
+  152     continue
+  151 continue
+C Load observation vector, b
+      do 154 i = 1,nobs
+          b(i,1) = obs(i,4)
+          b(i+nobs,1) = obs(i,5)
+          b(i+2*nobs,1) = obs(i,6)
+  154 continue
+C In 3-D, each observation corresponds to three rows in A, b
+      nobs = 3*nobs
+      nflt = 2*nflt
+      RETURN
+      END
 C----------------------------------------------------------------------C
 
       SUBROUTINE lsq(Ain,bin,xout,OBSMAX,PARMAX,nobs,npar)
@@ -313,14 +407,16 @@ C----------------------------------------------------------------------C
 
 C----------------------------------------------------------------------C
 
-      SUBROUTINE gcmdln(fltf,obsf,smooth,smoof,damp)
+      SUBROUTINE gcmdln(fltf,obsf,smooth,smoof,damp,dimen)
       IMPLICIT NONE
       CHARACTER*80 fltf,obsf,tag,smoof
       INTEGER i,narg
       REAL*8 smooth,damp
+      INTEGER dimen
       fltf = 'none'
       obsf = 'none'
       smoof = 'none'
+      dimen = 0
       smooth = -1.0d0
       damp = -1.0d0
       narg = iargc()
@@ -345,6 +441,10 @@ C----------------------------------------------------------------------C
               i = i + 1
               call getarg(i,tag)
               read(tag,'(BN,F10.0)') damp
+          elseif (tag(1:2).eq.'-2') then
+              dimen = 2
+          elseif (tag(1:2).eq.'-3') then
+              dimen = 3
           elseif (tag(1:2).eq.'-h') then
               call usage(' ')
           endif
@@ -365,7 +465,7 @@ C----------------------------------------------------------------------C
           write(*,*)
       endif
       write(*,*)
-     1 'Usage: fltinv -obs OBSFILE -flt FLTFILE [-2|-3] ',
+     1 'Usage: fltinv -obs OBSFILE -flt FLTFILE -2|-3 ',
      2         '[-LQR|SVD] [-smooth SMOOTH] [-smoof SMOOTHFILE] ',
      3         '[-damp DAMP] [-h]'
       write(*,*)
@@ -384,6 +484,9 @@ C----------------------------------------------------------------------C
       write(*,*)
      1 '                       3-D: x(m) y(m) z(m) str(deg) dip(deg) ',
      2                         'wid(m) len(m)'
+      write(*,*)
+     1 '-2|-3              Choose 2-D or 3-D problem (3-D in ',
+     2                                'development)'
       write(*,*)
      1 '-smooth CONST FILE Smooth model, with weight CONST, linking ',
      2                         'file FILE (format below)'
@@ -429,8 +532,6 @@ C----------------------------------------------------------------------C
      1 '                        *-----*-----*-----* '
       write(*,*)
      1 '-damp CONST         Minimize length of model, with weight CONST'
-      write(*,*)
-     1 '-2|-3              Choose 2-D or 3-D problem (ONLY 2D)'
       write(*,*)
      1 '-LQR|-SVD          Solve LS problem with QR/LQ factorization ',
      2                     'or SVD (NO SVD OPTION)'
