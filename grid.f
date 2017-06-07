8a6ff8ae50b11d8c23c0a81b543e18710003840f
Matthew Herman <mwh5316@psu.edu>
2017-06-07 16:19:55 -0500

    Modify grid.f
    1. Add option to clip output to polygon file.

--- grid.f
+++ grid.f
@@ -6,14 +6,15 @@ C----
       REAL*8 pi,d2r
       PARAMETER (pi=4.0d0*datan(1.0d0),d2r=pi/180.0d0)
       CHARACTER*40 ofile
+      CHARACTER*200 clip
       REAL*8 x1,x2,y1,y2,dx,dy,dz,dd,ref(5),dist,az,lo,la
-      REAL*8 x,y,z
-      INTEGER i,j,nx,ny,p,xsec,xz
+      REAL*8 x,y,z,xx(1000),yy(1000),zsave
+      INTEGER i,j,nx,ny,p,xsec,xz,nn,io
 
 C----
 C Get parameters from the command line
 C----
-      call gcmdln(x1,x2,nx,dx,y1,y2,ny,dy,z,ofile,p,ref,xsec,xz)
+      call gcmdln(x1,x2,nx,dx,y1,y2,ny,dy,z,ofile,p,ref,xsec,xz,clip)
 C Check that limits and increments are properly defined
       if (x2.lt.x1) call usage('!! Error: X2 must be greater than X1')
       if (y2.lt.y1) call usage('!! Error: Y2 must be greater than Y1')
@@ -32,6 +33,16 @@ C Check that limits and increments are properly defined
       endif
 C If output file unspecified, print result to standard output
       if (ofile.eq.'none'.and.p.eq.0) p = 1
+C If clip file is defined, read it
+      if (clip.ne.'none') then
+          open(unit=201,file=clip,status='old')
+          i = 1
+ 1001     read(201,*,end=1002) xx(i),yy(i)
+              i = i + 1
+              goto 1001
+ 1002     continue
+          nn = i - 1
+      endif
 
 C----
 C Calculate number of points and grid increments
@@ -77,6 +88,11 @@ C             CALCULATE Z ON DIPPING GRID
                   dz = dd*dtan(ref(5)*d2r)
                   z = ref(3) + dz
               endif
+              if (clip.ne.'none') then
+                  zsave = z
+                  call pnpoly(x,y,xx,yy,nn,io)
+                  if (io.eq.-1) z = -12345d0
+              endif
               if (p.eq.0.and.xsec.eq.0) then
                   write(101,9999) x,y,z
               elseif (p.eq.1.and.xsec.eq.0) then
@@ -90,6 +106,7 @@ C             CALCULATE Z ON DIPPING GRID
               elseif (p.eq.1.and.xsec.eq.1.and.xz.eq.1) then
                   write(*,9999) x,y
               endif
+              z = zsave
    15     continue
    16 continue
       stop
@@ -184,10 +201,98 @@ C     Return input values in initial units
       END
 
 C----------------------------------------------------------------------C
+C                                                                       
+C        SUBROUTINE PNPOLY                                              
+C                                                                       
+C        PURPOSE                                                        
+C           TO DETERMINE WHETHER A POINT IS INSIDE A POLYGON            
+C                                                                       
+C        USAGE                                                          
+C           CALL PNPOLY (PX, PY, XX, YY, N, INOUT )                     
+C                                                                       
+C        DESCRIPTION OF THE PARAMETERS                                  
+C           PX      - X-COORDINATE OF POINT IN QUESTION.                
+C           PY      - Y-COORDINATE OF POINT IN QUESTION.                
+C           XX      - N LONG VECTOR CONTAINING X-COORDINATES OF         
+C                     VERTICES OF POLYGON.                              
+C           YY      - N LONG VECTOR CONTAING Y-COORDINATES OF           
+C                     VERTICES OF POLYGON.                              
+C           N       - NUMBER OF VERTICES IN THE POLYGON.                
+C           INOUT   - THE SIGNAL RETURNED:                              
+C                     -1 IF THE POINT IS OUTSIDE OF THE POLYGON,        
+C                      0 IF THE POINT IS ON AN EDGE OR AT A VERTEX,     
+C                      1 IF THE POINT IS INSIDE OF THE POLYGON.         
+C                                                                       
+C        REMARKS                                                        
+C           THE VERTICES MAY BE LISTED CLOCKWISE OR ANTICLOCKWISE.      
+C           THE FIRST MAY OPTIONALLY BE REPEATED, IF SO N MAY           
+C           OPTIONALLY BE INCREASED BY 1.                               
+C           THE INPUT POLYGON MAY BE A COMPOUND POLYGON CONSISTING      
+C           OF SEVERAL SEPARATE SUBPOLYGONS. IF SO, THE FIRST VERTEX    
+C           OF EACH SUBPOLYGON MUST BE REPEATED, AND WHEN CALCULATING   
+C           N, THESE FIRST VERTICES MUST BE COUNTED TWICE.              
+C           INOUT IS THE ONLY PARAMETER WHOSE VALUE IS CHANGED.         
+C           THE SIZE OF THE ARRAYS MUST BE INCREASED IF N > MAXDIM      
+C           WRITTEN BY RANDOLPH FRANKLIN, UNIVERSITY OF OTTAWA, 7/70.   
+C                                                                       
+C        SUBROUTINES AND FUNCTION SUBPROGRAMS REQUIRED                  
+C           NONE                                                        
+C                                                                       
+C        METHOD                                                         
+C           A VERTICAL LINE IS DRAWN THRU THE POINT IN QUESTION. IF IT  
+C           CROSSES THE POLYGON AN ODD NUMBER OF TIMES, THEN THE        
+C           POINT IS INSIDE OF THE POLYGON.                             
+C                                                                       
+C     ..................................................................
+C                                                                       
+      SUBROUTINE PNPOLY(PX,PY,XX,YY,N,INOUT)
+      IMPLICIT NONE
+      REAL*8 X(1000),Y(1000),XX(N),YY(N),PX,PY
+      INTEGER MAXDIM,N,INOUT,I,J
+      LOGICAL MX,MY,NX,NY
 
-      SUBROUTINE gcmdln(x1,x2,nx,dx,y1,y2,ny,dy,z,ofile,p,ref,xsec,xz)
+      MAXDIM=1000
+      IF (N.GT.MAXDIM) THEN
+          WRITE(0,7) N
+7         FORMAT('WARNING:',I5,' TOO GREAT FOR THIS VERSION OF PNPOLY')
+          RETURN
+      ENDIF
+
+      DO 1 I=1,N
+          X(I)=XX(I)-PX
+          Y(I)=YY(I)-PY
+    1 CONTINUE
+
+      INOUT=-1
+      DO 2 I=1,N
+          J=1+MOD(I,N)
+          MX=X(I).GE.0.0
+          NX=X(J).GE.0.0
+          MY=Y(I).GE.0.0
+          NY=Y(J).GE.0.0
+          IF(.NOT.((MY.OR.NY).AND.(MX.OR.NX)).OR.(MX.AND.NX)) THEN
+              GOTO 2
+          ENDIF
+          IF(.NOT.(MY.AND.NY.AND.(MX.OR.NX).AND..NOT.(MX.AND.NX))) THEN
+              GOTO 3
+          ENDIF
+          INOUT=-INOUT
+          GOTO 2
+3         IF ((Y(I)*X(J)-X(I)*Y(J))/(X(J)-X(I))) 2,4,5
+4         INOUT=0
+          RETURN
+5         INOUT=-INOUT
+2     CONTINUE
+      RETURN
+      END
+
+C----------------------------------------------------------------------C
+
+      SUBROUTINE gcmdln(x1,x2,nx,dx,y1,y2,ny,dy,z,ofile,p,ref,xsec,xz,
+     1                  clip)
       IMPLICIT none
       CHARACTER*40 tag,ofile
+      CHARACTER*200 clip
       REAL*8 x1,x2,y1,y2,z,dx,dy,ref(5)
       INTEGER narg,i,j,nx,ny,p,xsec,xz
 C Initialize variable values
@@ -204,6 +309,7 @@ C Initialize variable values
       p = 0
       xsec = 0
       xz = 0
+      clip = 'none'
       do 100 i = 1,5
           ref(i) = -1.0d0
   100 continue
@@ -224,6 +330,9 @@ C Initialize variable values
           i = i + 3
       elseif (tag(1:3).eq.'-xz') then
           xz = 1
+      elseif (tag(1:5).eq.'-clip') then
+          i = i + 1
+          call getarg(i,clip)
       elseif (tag(1:2).eq.'-x') then
           i = i + 1
           call getarg(i,tag)
@@ -292,43 +401,47 @@ C----------------------------------------------------------------------C
       endif
       write(*,*)
      1 'Usage: grid -x X1 X2 [-nx NX|-dx DX] ',
-     2            '-y Y1 Y2 [-ny NY|-dy DY]'
+     2            '-y Y1 Y2 [-ny NY|-dy DY] '
       write(*,*)
      1 '            [-z Z|-dip X0 Y0 Z0 STR ',
-     2              'DIP] [-xsec X0 Y0 AZ] [-xz]'
+     2              'DIP|-xsec X0 Y0 AZ [-xz]]'
       write(*,*)
-     1 '            [-o OFILE] [-p] [-h]'
+     1 '            [-clip XYFILE] [-o OFILE] [-p] [-h]'
       write(*,*)
       write(*,*)
-     1 '-x X1 X2    First column (x) limits'
+     1 '-x X1 X2               First column (x) limits'
       write(*,*)
-     1 '-nx NX      Number of x grid points'
+     1 '-nx NX                 Number of x grid points'
       write(*,*)
-     1 '-dx DX      Increment in x direction (overrides -nx)'
+     1 '-dx DX                 Increment in x direction (overrides -nx)'
       write(*,*)
-     1 '-y Y1 Y2    Second column (y) limits'
+     1 '-y Y1 Y2               Second column (y) limits'
       write(*,*)
-     1 '-ny NY      Number of y grid points'
+     1 '-ny NY                 Number of y grid points'
       write(*,*)
-     1 '-dy DY      Increment in y direction (overrides -ny)'
+     1 '-dy DY                 Increment in y direction (overrides -ny)'
       write(*,*)
-     1 '-z Z        Third column (z) value'
+     1 '-z Z                   Third column (z) value'
       write(*,*)
-     1 '-dip X0 Y0 Z0 STR DIP   Put grid onto plane with STR/DIP ',
+     1 '-dip X0 Y0 Z0 STR DIP  Put grid onto plane with STR/DIP ',
      2                'and containing (X0,Y0,Z0)'
       write(*,*)
-     1 '-xsec X0 Y0 AZ          Create a vertical cross section ',
+     1 '-xsec X0 Y0 AZ         Create a vertical cross section ',
      2                 'through (X0,Y0) with strike AZ'
       write(*,*)
-     1 '-xz                     Print cross section x-z instead of ',
+     1 '-xz                    Print cross section x-z instead of ',
      2                            'lon lat z'
       write(*,*)
-     1 '-o OFILE    Output to file (default prints to standard ',
-     2              'output)'
+     1 '-clip XYFILE           Clip points to lie in interior of ',
+     2                            'XYFILE (outside points are nan)'
+      write(*,*)
+     1 '-o OFILE               Output to file (default prints to ',
+     2              'standard output)'
       write (*,*)
-     1 '-p          Print results to standard output (overrides -o)'
+     1 '-p                     Print results to standard output ',
+     2                               '(overrides -o)'
       write (*,*)
-     1 '-h/-?       Online help (this screen)'
+     1 '-h/-?                  Online help (this screen)'
       write (*,*)
       STOP
       END
