faff687da31eadeadfe4a742b70b3ae84eddfd49
mherman09 <matthew.w.herman@gmail.com>
2019-04-03 14:52:38 -0400

    Modify test_module.f90, add error_exit.c
    1. Add C subroutine to exit a Fortran program with non-zero
       exit code.
    2. Update test subroutines to exit using error_exit().
    3. Automatically determine variable type in test_value().

---
+++ src/error_exit.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+void error_exit_(Status)
+
+int *Status;
+{
+  exit(*Status);
+}
--- src/test_module.f90
+++ src/test_module.f90
@@ -1,13 +1,24 @@
 module test
 
+interface test_value
+    module procedure test_value_dp
+    module procedure test_value_int
+    module procedure test_value_real
+end interface
+
 !--------------------------------------------------------------------------------------------------!
 contains
 !--------------------------------------------------------------------------------------------------!
 
-subroutine test_value(actual_value,expected_value,string)
+subroutine test_value_dp(actual_value,expected_value,string)
 implicit none
-double precision :: actual_value, expected_value, diff
+
+! Arguments
+double precision :: actual_value, expected_value
 character(len=*) :: string
+
+! Local variables
+double precision :: diff
 
 diff = (actual_value-expected_value)/actual_value
 
@@ -15,7 +26,52 @@ if (dabs(diff).gt.1.0d-10) then
     write(0,*) 'test FAILED for '//trim(string)
     write(0,*) '    expected value: ',expected_value
     write(0,*) '    computed value: ',actual_value
-    stop
+    call error_exit(1)
+else
+    write(6,*) 'test passed for '//trim(string)
+endif
+return
+end subroutine
+
+!--------------------------------------------------------------------------------------------------!
+
+subroutine test_value_int(actual_value,expected_value,string)
+implicit none
+
+! Arguments
+integer :: actual_value, expected_value
+character(len=*) :: string
+
+if (actual_value.ne.expected_value) then
+    write(0,*) 'test FAILED for '//trim(string)
+    write(0,*) '    expected value: ',expected_value
+    write(0,*) '    computed value: ',actual_value
+    call error_exit(1)
+else
+    write(6,*) 'test passed for '//trim(string)
+endif
+return
+end subroutine
+
+!--------------------------------------------------------------------------------------------------!
+
+subroutine test_value_real(actual_value,expected_value,string)
+implicit none
+
+! Arguments
+real :: actual_value, expected_value
+character(len=*) :: string
+
+! Local variables
+real :: diff
+
+diff = (actual_value-expected_value)/actual_value
+
+if (abs(diff).gt.1.0e-7) then
+    write(0,*) 'test FAILED for '//trim(string)
+    write(0,*) '    expected value: ',expected_value
+    write(0,*) '    computed value: ',actual_value
+    call error_exit(1)
 else
     write(6,*) 'test passed for '//trim(string)
 endif
