2ab1d5cd70f217c9e9ecc227ad32cf4223bdfd9c
mherman09 <matthew.w.herman@gmail.com>
2019-06-04 21:04:30 +0200

    Add program fitutil
    1. Program to fit x-y data with a polynomial, sinusoid, and/or
       an exponential function.

--- src/fitutil.f90
+++ src/fitutil.f90
@@ -1,11 +1,22 @@
-module polyfit
+module fitutil
 
 character(len=512) :: input_file
 character(len=512) :: output_file
 
 integer :: poly_order
-logical :: period(4)
-logical :: doExp
+double precision :: period(4)
+integer :: nperiod
+double precision :: exp_const(4)
+integer :: nexponent
+logical :: doPolynomial
+logical :: doSinusoid
+logical :: doExponential
+logical :: findExpConstant
+
+logical :: labelCoefficients
+
+character(len=512) :: pre_file
+logical :: printPredicted
 
 end module
 
@@ -13,231 +24,404 @@ end module
 
 program main
 
+use io, only: stdin, stdout, stderr, fileExists, line_count
+use trig, only: pi
+use solver, only: solve_dgels
+
+use fitutil, only: input_file, &
+                   output_file, &
+                   poly_order, &
+                   period, &
+                   nperiod, &
+                   exp_const, &
+                   nexponent, &
+                   doPolynomial, &
+                   doSinusoid, &
+                   doExponential, &
+                   findExpConstant, &
+                   labelCoefficients, &
+                   pre_file, &
+                   printPredicted
+
 implicit none
 
-!       PROGRAM polyfit
-!       IMPLICIT none
-!       CHARACTER*40 ifile,ofile,cfile,pfile,rfile,MXORDC
-!       LOGICAL ex
-!       INTEGER i,nobs,MXOBS
-!       PARAMETER (MXOBS=100000)
-!       REAL*8 xobs(MXOBS),yobs(MXOBS)
-!       INTEGER MXORD,ord,MXCON,ncon
-!       PARAMETER (MXORD=20,MXCON=21)
-!       REAL*8 coeff(MXORD),xc(MXCON),yc(MXCON),ypre,res
-!       INTEGER j,p
-! C----
-! C TO-DO:
-! C Subroutine chkopt(ifile,ofile,cfile,ord,MXORD)
-! C   check existence of input files
-! C   check output file defined
-! C   check ord <= MXORD
-! C Subroutine readxy(ifile,xobs,yobs,nobs)
-! C   read in values and number of observations from ifile
-! C Subroutine readconstr(cfile,xc,yc,ncon)
-! C   read in constraints if necessary
-! C----
-!
-! #ifndef USELAPACK
-!       write(0,*) 'polyfit: compile with LAPACK to use'
-!       stop
-! #endif
-!
-! C----
-! C Parse command line
-! C----
-!       call gcmdln(ifile,ofile,cfile,xc,yc,ncon,pfile,rfile,ord,p)
-!
-! C----
-! C Check for required input files and values
-! C----
-!       if (ifile.eq.'none') then
-!           write(*,*) '!! Error: Input x-y data is not specified'
-!           call usage('!! Use -f XYFILE to specify input file')
-!       elseif (ifile.ne.'stdin') then
-!           inquire(file=ifile,EXIST=ex)
-!           if (.not.ex) call usage('!! Error: no x-y file: '//ifile)
-!       endif
-!       if (ord.lt.0) then
-!           write(*,*) '!! Error: polynomial order not specified'
-!           call usage('!! Use -ord ORDER to specify order')
-!       endif
-!       if (ofile.eq.'none'.and.p.eq.0) then
-!           write(*,*) '!! Error: No output file specified'
-!           call usage('!! Use -o COEFF to specify coefficient file')
-!       endif
-! C Check for optional input files and values
-!       if (cfile.ne.'none') then
-!           inquire(file=cfile,EXIST=ex)
-!           if (.not.ex) call usage('!! Error: no constraint file: '
-!      1                                           //cfile)
-!       endif
-!       if (ord.gt.MXORD) then
-!           write(MXORDC,*) MXORD
-!           call usage('!! Error: ORD must be <='//MXORDC)
-!       endif
-!
-! C----
-! C Extract observed data from input time series
-! C----
-!       if (ifile.ne.'stdin') then
-!           open(unit=11,file=ifile,status='old')
-!           i = 1
-!   101     read(11,*,end=102) xobs(i),yobs(i)
-!               i = i + 1
-!               goto 101
-!   102     continue
-!           close(11)
-!           nobs = i - 1
-!       else
-!           i = 1
-!   111     read(*,*,end=112) xobs(i),yobs(i)
-!               i = i + 1
-!               goto 111
-!   112     continue
-!           nobs = i - 1
-!       endif
-! C----
-! C Check if a constraint file was defined, and compute coefficients
-! C----
-! C     Unconstrained least squares fit
-!       if (cfile.eq.'none'.and.xc(1).lt.-1.0d98) then
-!           call polylsq(coeff,xobs,yobs,nobs,ord)
-! C     Constrained least squares fit
-!       elseif (cfile.eq.'none') then
-!           call polylsqconstr(coeff,xobs,yobs,nobs,ord,xc,yc,ncon)
-!       else
-!           open(unit=13,file=cfile,status='old')
-!           i = 1
-!  103      read(13,*,end=104) xc(i),yc(i)
-!               i = i + 1
-!               goto 103
-!  104      continue
-!           close(13)
-!           ncon = i - 1
-!           call polylsqconstr(coeff,xobs,yobs,nobs,ord,xc,yc,ncon)
-!       endif
-!
-! C----
-! C Print coefficients
-! C----
-!       if (p.eq.0) then
-!           open(unit=14,file=ofile,status='unknown')
-!           write(14,9001)
-!           do 105 i = 1,ord+1
-!               write(14,9002) i-1,coeff(i)
-!   105     continue
-!           close(14)
-!       else
-!           write(*,9001)
-!           do 106 i = 1,ord+1
-!               write(*,9002) i-1,coeff(i)
-!   106     continue
-!       endif
-!
-! C----
-! C Print predicted results
-! C----
-!       if (pfile.ne.'none') then
-!           open(unit=12,file=pfile,status='unknown')
-!           do 108 i = 1,nobs
-!               ypre = 0.0d0
-!               do 107 j = 1,ord+1
-!                   ypre = ypre + coeff(j)*xobs(i)**(j-1)
-!   107         continue
-!               write (12,9003) xobs(i),ypre
-!   108     continue
-!           close(12)
-!       endif
-!
-!       if (rfile.ne.'none') then
-!           open(unit=15,file=rfile,status='unknown')
-!           do 110 i = 1,nobs
-!               ypre = 0.0d0
-!               do 109 j = 1,ord+1
-!                   ypre = ypre + coeff(j)*xobs(i)**(j-1)
-!   109         continue
-!               res = yobs(i) - ypre
-!               write(15,9003) xobs(i),res
-!   110     continue
-!       endif
-!
-!  9001 format('POLY_ORDER      COEFFICIENT')
-!  9002 format(I10,X,1PE16.6)
-!  9003 format(2E14.6)
+! Local variables
+integer :: i, j, ios, ierr, nrows, ncols, ptr_poly, ptr_sin, ptr_exp, npoly, nsin, nexp, luin, luout
+integer :: iperiod, iexponent
+integer, parameter :: NROWS_STDIN = 100000
+character(len=512) :: line
+double precision, allocatable :: obs(:,:), A(:,:), b(:), x(:)
+double precision :: pre, amp, pha
+
+
+#ifndef USE_LAPACK
+    call usage('fitutil: does not work if compiled without LAPACK libraries')
+#endif
+
+
+! Parse command line
+call gcmdln()
+
+! Make sure something needs to be fit...
+if (.not.doPolynomial .and. .not.doSinusoid .and. .not.doExponential) then
+    call usage('fitutil: no fitting function specified')
+endif
+
+
+! Determine input/output streams
+if (input_file.eq.''.or.input_file.eq.'none'.or.input_file.eq.'stdin') then
+    luin = stdin
+else
+    if (.not.fileExists(input_file)) then
+        call usage('fitutil: no file found named "'//trim(input_file)//'"')
+    endif
+    luin = 11
+endif
+
+if (output_file.eq.''.or.output_file.eq.'none'.or.output_file.eq.'stdout') then
+    luout = stdout
+else
+    luout = 12
+    open(unit=luout,file=output_file,status='unknown')
+endif
+
+
+! Calculate number of parameters to fit and set up pointers to array
+ncols = 0
+ptr_poly = 0
+ptr_sin = 0
+ptr_exp = 0
+npoly = 0
+nsin = 0
+nexp = 0
+
+if (doPolynomial) then
+    ptr_poly = ncols + 1
+    npoly = poly_order + 1
+    ncols = ncols + npoly
+endif
+
+if (doSinusoid) then
+    ptr_sin = ncols + 1
+    nsin = 2*nperiod
+    ncols = ncols + nsin
+endif
+
+if (doExponential) then
+    ptr_exp = ncols + 1
+    if (findExpConstant) then
+        if (ptr_exp.gt.1) then
+            call usage('fitutil: cannot linearize model matrix with exponential and polynomial '//&
+                       'or sinusoid')
+        endif
+        nexp = 2
+    else
+        nexp = nexponent
+    endif
+    ncols = ncols + nexp
+endif
+
+
+! Count number of input values and read into obs array
+nrows = 0
+if (luin.eq.stdin) then
+    allocate(obs(NROWS_STDIN,2))
+    do
+        read(luin,'(A)',iostat=ios) line
+        if (ios.ne.0) then
+            exit
+        else
+            nrows = nrows + 1
+            if (nrows.gt.NROWS_STDIN) then
+                write(stderr,*) 'fitutil: max number of lines using stdin is ',NROWS_STDIN
+                call usage(     'Read input from file to increase number of inputs')
+            endif
+            read(line,*,iostat=ios) obs(nrows,1),obs(nrows,2)
+            if (ios.ne.0) then
+                write(stderr,*) 'fitutil: error parsing x-y from line ',nrows
+                call usage(     'Offending line: '//trim(line))
+            endif
+        endif
+    enddo
+
+else
+    nrows = line_count(input_file)
+    open(unit=luin,file=input_file,status='old')
+    allocate(obs(nrows,2))
+    do i = 1,nrows
+        read(luin,'(A)',iostat=ios,err=9001,end=9001) line
+        read(line,*,iostat=ios) obs(i,1),obs(i,2)
+        9001 if (ios.ne.0) then
+            write(stderr,*) 'fitutil: error parsing x-y from line ',i
+            call usage(     'Offending line: '//trim(line))
+        endif
+    enddo
+endif
+
+
+! Load model matrix and data vector
+
+! Allocate memory to model matrix, data vector, and solution vector
+allocate(A(nrows,ncols))
+allocate(b(nrows))
+allocate(x(ncols))
+
+! Load arrays
+do i = 1,nrows
+    if (doPolynomial) then
+        do j = 1,poly_order+1
+            A(i,j) = obs(i,1)**(j-1)
+        enddo
+    endif
+
+    if (doSinusoid) then
+        do j = 1,nperiod
+            A(i,ptr_sin+2*(j-1)) = sin(2.0d0*pi/period(j)*obs(i,1))
+            A(i,ptr_sin+2*(j-1)+1) = cos(2.0d0*pi/period(j)*obs(i,1))
+        enddo
+    endif
+
+    if (doExponential) then
+        if (findExpConstant) then
+            A(i,ptr_exp) = obs(i,1)
+            A(i,ptr_exp+1) = 1.0d0
+        else
+            do j = 1,nexponent
+                A(i,ptr_exp+j-1) = exp(exp_const(j)*obs(i,1))
+            enddo
+        endif
+    endif
+
+    b(i) = obs(i,2)
+enddo
+
+if (doExponential.and.findExpConstant) then
+    b = log(b)
+endif
+
+
+! Solve system of equations Ax = b for x
+call solve_dgels(A,b,x,nrows,ncols,ierr)
+
+
+! Write results to file or standard output
+i = 1
+do while (i.le.ncols)
+    ! Polynomial
+    if (i.le.ptr_poly+npoly-1) then
+        if (labelCoefficients) then
+            write(luout,'(" Order_",I0.2,X,1PE16.8)') i-1,x(i)
+        else
+            write(luout,*) x(i)
+        endif
+
+    ! Sinusoid
+    elseif (i.le.ptr_sin+nsin-1) then
+        amp = sqrt(x(i)**2+x(i+1)**2)
+        pha = atan2(x(i+1),x(i))
+        if (labelCoefficients) then
+            write(luout,'(" SinAmplitude_",I0.2,X,1PE16.8)') i-ptr_sin+1,amp
+            write(luout,'(" SinPhase_",I0.2,X,1PE16.8)') i-ptr_sin+1,pha
+        else
+            write(luout,*) amp
+            write(luout,*) pha
+        endif
+        i = i + 1
+
+    ! Exponential
+    elseif (i.le.ptr_exp+nexp-1) then
+        if (findExpConstant) then
+            if (labelCoefficients) then
+                write(luout,'(" ExpAmplitude_",I0.2,X,1PE16.8)') i,exp(x(i+1))
+                write(luout,'(" ExpConst_",I0.2,X,1PE16.8)') i,x(i)
+            else
+                write(luout,*) exp(x(i+1))
+                write(luout,*) x(i)
+            endif
+            i = i + 1
+        else
+            if (labelCoefficients) then
+                write(luout,'( " ExpAmplitude_",I0.2,X,1PE16.8)') i-ptr_exp+1,x(i)
+            else
+                write(luout,*) x(i)
+            endif
+        endif
+    endif
+    i = i + 1
+enddo
+
+
+! Print predicted results
+if (printPredicted) then
+    open(unit=31,file=pre_file,status='unknown')
+    do i = 1,nrows
+        pre = 0.0d0
+        j = 1
+        do while (j.le.ncols)
+            ! Polynomial
+            if (j.le.ptr_poly+npoly-1) then
+                pre = pre + x(j)*obs(i,1)**(j-1)
+
+            ! Sinusoid
+            elseif (j.le.ptr_sin+nsin-1) then
+                iperiod = (j-ptr_sin)/nsin + 1
+                amp = sqrt(x(j)**2+x(j+1)**2)
+                pha = atan2(x(j+1),x(j))
+                pre = pre + amp*sin(2.0d0*pi/period(iperiod)*obs(i,1)+pha)
+                j = j + 1
+
+            ! Exponential
+            elseif (j.le.ptr_exp+nexp-1) then
+                if (findExpConstant) then
+                    pre = pre + exp(x(i+1))*exp(x(i)*obs(i,1))
+                    j = j + 1
+                else
+                    iexponent = (j-ptr_exp)/nexponent+1
+                    pre = pre + x(j)*exp(exp_const(iexponent)*obs(i,1))
+                endif
+            endif
+            j = j + 1
+        enddo
+        write(31,*) pre
+    enddo
+    close(31)
+endif
+
+
+if (luin.ne.stdin) then
+    close(11)
+endif
+if (luout.ne.stdout) then
+    close(12)
+endif
 
 end
 
 !--------------------------------------------------------------------------------------------------!
 
 subroutine gcmdln()
+
+use fitutil, only: input_file, &
+                   output_file, &
+                   poly_order, &
+                   period, &
+                   nperiod, &
+                   exp_const, &
+                   nexponent, &
+                   doPolynomial, &
+                   doSinusoid, &
+                   doExponential, &
+                   findExpConstant, &
+                   labelCoefficients, &
+                   pre_file, &
+                   printPredicted
+
+
 implicit none
-! C======================================================================C
-!
-!       SUBROUTINE gcmdln(ifile,ofile,cfile,xc,yc,ncon,pfile,rfile,ord,p)
-!       IMPLICIT NONE
-!       INTEGER MXCON
-!       PARAMETER (MXCON=21)
-!       REAL*8 xc(MXCON),yc(MXCON)
-!       CHARACTER*40 ifile,ofile,cfile,pfile,rfile,tag
-!       INTEGER i,j,narg,ord,p,ncon
-!       j = 0
-!       xc(1) = -1.0d99
-!       ifile = 'none'
-!       ofile = 'none'
-!       cfile = 'none'
-!       pfile = 'none'
-!       rfile = 'none'
-!       p = 0
-!       ord = -1
-!       ncon = 0
-!       narg = iargc()
-!       if (narg.eq.0) then
-!           call usage('')
-!       endif
-!       i = 0
-!   101 i = i + 1
-!       if (i.gt.narg) goto 102
-!           call getarg(i,tag)
-!           if (tag(1:4).eq.'-pre') then
-!               i = i + 1
-!               call getarg(i,pfile)
-!           elseif (tag(1:4).eq.'-ord') then
-!               i = i + 1
-!               call getarg(i,tag)
-!               read(tag,'(BN,I5)') ord
-!           elseif (tag(1:4).eq.'-res') then
-!               i = i + 1
-!               call getarg(i,rfile)
-!           elseif (tag(1:3).eq.'-cf') then
-!               i = i + 1
-!               call getarg(i,cfile)
-!           elseif (tag(1:2).eq.'-c') then
-!               j = j + 1
-!               i = i + 1
-!               call getarg(i,tag)
-!               read(tag,'(BN,F10.0)') xc(j)
-!               i = i + 1
-!               call getarg(i,tag)
-!               read(tag,'(BN,F10.0)') yc(j)
-!           elseif (tag(1:2).eq.'-p') then
-!               p = 1
-!           elseif (tag(1:2).eq.'-o') then
-!               i = i + 1
-!               call getarg(i,ofile)
-!           elseif (tag(1:2).eq.'-f') then
-!               i = i + 1
-!               call getarg(i,ifile)
-!           elseif (tag(1:2).eq.'-h') then
-!               call usage(' ')
-!           else
-!               call usage('!! Error: No option '//tag)
-!           endif
-!           goto 101
-!   102 continue
-!       ncon = j
-!       RETURN
-!       END
+
+! Local variables
+integer :: i, ios, narg
+character(len=512) :: tag
+double precision :: dp
+
+
+input_file = ''
+output_file = ''
+poly_order = 0
+period = 0.0d0
+nperiod = 0
+exp_const = 0.0d0
+nexponent = 0
+doPolynomial = .false.
+doSinusoid = .false.
+doExponential = .false.
+findExpConstant = .false.
+labelCoefficients = .true.
+pre_file = ''
+printPredicted = .false.
+
+! Number of arguments
+narg = command_argument_count()
+if (narg.eq.0) then
+  call usage('')
+endif
+
+! Read arguments
+i = 1
+do while (i.le.narg)
+
+    call get_command_argument(i,tag)
+
+    if (trim(tag).eq.'-f') then
+        i = i + 1
+        call get_command_argument(i,input_file)
+
+    elseif (tag.eq.'-o') then
+        i = i + 1
+        call get_command_argument(i,output_file)
+
+    elseif (tag.eq.'-poly') then
+        i = i + 1
+        call get_command_argument(i,tag)
+        read(tag,*) poly_order
+        doPolynomial = .true.
+
+    elseif (tag.eq.'-sin') then
+        i = i + 1
+        call get_command_argument(i,tag)
+        nperiod = nperiod + 1
+        if (nperiod.gt.4) then
+            call usage('fitutil: maximum number of sinusoidal periods is 4')
+        endif
+        read(tag,*) period(nperiod)
+        doSinusoid = .true.
+
+    elseif (tag.eq.'-exp') then
+        ! Default: fit single exponential amplitude and exponential constant to data
+        findExpConstant = .true.
+
+        ! Check if the exponential constant is fixed
+        i = i + 1
+        if (i.le.narg) then
+            call get_command_argument(i,tag)
+            read(tag,*,iostat=ios) dp
+            if (ios.eq.0) then
+                ! If this can be read as a real number, then the exponential constant is fixed
+                nexponent = nexponent + 1
+                if (nexponent.gt.4) then
+                    call usage('fitutil: maximum number of exponential constants is 4')
+                endif
+                read(tag,*,iostat=ios) exp_const(nexponent)
+                findExpConstant = .false.
+            else
+                i = i - 1
+            endif
+        endif
+
+        doExponential = .true.
+
+    elseif (tag.eq.'-label_coeff') then
+        i = i + 1
+        call get_command_argument(i,tag)
+        if (tag.eq.'Y'.or.tag.eq.'y'.or.tag.eq.'1'.or.tag.eq.'on') then
+            labelCoefficients = .true.
+        elseif (tag.eq.'N'.or.tag.eq.'n'.or.tag.eq.'0'.or.tag.eq.'off') then
+            labelCoefficients = .false.
+        endif
+
+    elseif (tag.eq.'-pre') then
+        i = i + 1
+        call get_command_argument(i,pre_file)
+        printPredicted = .true.
+
+    else
+        call usage('fitutil: no option '//trim(tag))
+    endif
+
+    i = i + 1
+
+enddo
+
 return
 end subroutine
 
@@ -256,15 +440,15 @@ if (str.ne.'') then
     write(stderr,*)
 endif
 
-write(stderr,*) 'Usage: polyfit ...options...'
+write(stderr,*) 'Usage: fitutil ...options...'
 write(stderr,*)
 write(stderr,*) '-f FILE           X-Y data to fit (default: stdin)'
 write(stderr,*) '-o FILE           Inverted coefficients (default: stdout)'
 write(stderr,*) '-poly N           Polynomial fit of order N'
-write(stderr,*) '-sin T            Sinusoid fit with period T'
-write(stderr,*) '-exp              Exponential fit'
-write(stderr,*) '-constraint FILE  Fixed coordinates'
-write(stderr,*) '-pre FILE         Predicted values at input x-coordinates'
+write(stderr,*) '-sin T            Sinusoid fit with period T (repeat up to 4 times)'
+write(stderr,*) '-exp [TAU]        Exponential fit (optional: up to 4 fixed exponential constants)'
+write(stderr,*) '-label_coeff Y|N  Label output coefficients (default: Y)'
+write(stderr,*) '-pre PFILE        Predicted values at input x-coordinates'
 write(stderr,*)
 
 stop
---
+++ test/test_fitutil.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Fit quadratic polynomial
+C0="4.6"
+C1="-1.2"
+cat > xy.tmp << EOF
+1.0000000000000000 3.4
+2.0000000000000000 2.2
+3.0000000000000000 1
+4.0000000000000000 -0.2
+5.0000000000000000 -1.4
+EOF
+cat > answer.tmp << EOF
+   4.6000000000000014     
+  -1.2000000000000004 
+EOF
+../bin/fitutil -f xy.tmp -poly 1 -label_coeff 0 > poly_coeff.tmp
+test_values.sh poly_coeff.tmp answer.tmp 1 "fitutil: 1st order polynomial" || exit 1
+
+# Fit sinusoid
+PERIOD="6.3"
+AMPLITUDE="7.7"
+PHASE="2.1"
+cat > xy.tmp << EOF
+-16.000000000000000 -7.4004
+-15.000000000000000 -2.22834
+-14.000000000000000 4.98245
+-13.000000000000000 7.63476
+-12.000000000000000 3.30197
+-11.000000000000000 -4.05181
+-10.000000000000000 -7.69856
+-9.0000000000000000 -4.30184
+-8.0000000000000000 3.03067
+-7.0000000000000000 7.59039
+-6.0000000000000000 5.20561
+-5.0000000000000000 -1.94183
+-4.0000000000000000 -7.31267
+-3.0000000000000000 -5.9931
+-2.0000000000000000 0.809603
+-1.0000000000000000 6.87159
+0.0000000000000000 6.64671
+1.0000000000000000 0.340703
+2.0000000000000000 -6.27702
+3.0000000000000000 -7.15185
+4.0000000000000000 -1.4834
+5.0000000000000000 5.54222
+6.0000000000000000 7.49722
+7.0000000000000000 2.59296
+EOF
+cat > answer.tmp << EOF
+   7.6999993020947040     
+   2.0999999661540500 
+EOF
+../bin/fitutil -f xy.tmp -sin $PERIOD -label_coeff 0 -o sin_coeff.tmp
+test_values.sh sin_coeff.tmp answer.tmp 1 "fitutil: sinusoid" || exit 1
+
+# Fit exponential with free exponential constant
+A="4.6"
+C="-1.2"
+cat > xy.tmp << EOF
+1.0000000000000000 1.38549
+2.0000000000000000 0.417303
+3.0000000000000000 0.125689
+4.0000000000000000 0.0378568
+5.0000000000000000 0.0114023
+EOF
+../bin/fitutil -f xy.tmp -exp -label_coeff n > exp_coeff.tmp
+cat > answer.tmp << EOF
+ 4.59998641E+00
+-1.19999901E+00
+EOF
+test_values.sh exp_coeff.tmp answer.tmp 1 "fitutil: exponential, find exponential constant" || exit 1
+
+# Fit exponential with fixed exponential constant
+../bin/fitutil -f xy.tmp -exp $C -label_coeff n > exp_coeff.tmp
+cat > answer.tmp << EOF
+   4.5999901531556864
+EOF
+test_values.sh exp_coeff.tmp answer.tmp 1 "fitutil: exponential, fixed exponential constant" || exit 1
+
+# Fit linear + sinusoid + exponential, print predicted values
+C0=-5.2
+C1=0.6
+A=1.5
+T=4.2
+PHASE=0.6
+AEXP=4.2
+CEXP=-0.7
+cat > xy.tmp << EOF
+-3.0000000000000000 28.3164
+-2.0000000000000000 9.60982
+-1.0000000000000000 1.48651
+0.0000000000000000 -0.153036
+1.0000000000000000 -1.21651
+2.0000000000000000 -3.61728
+3.0000000000000000 -4.28111
+4.0000000000000000 -2.10017
+5.0000000000000000 -0.611316
+6.0000000000000000 -1.76296
+7.0000000000000000 -2.46435
+8.0000000000000000 -0.382067
+9.0000000000000000 1.7037
+10.000000000000000 1.02502
+11.000000000000000 -0.0610226
+12.000000000000000 1.56111
+13.000000000000000 3.99766
+14.000000000000000 3.84889
+15.000000000000000 2.49988
+16.000000000000000 3.55706
+17.000000000000000 6.17427
+EOF
+../bin/fitutil -f xy.tmp -poly 1 -sin $T -exp $CEXP -label_coeff 0 -pre pre.tmp > multi.tmp
+cat > answer.tmp << EOF
+  -5.1999990119741932     
+  0.59999992890469644     
+   1.4999997983110380     
+  0.60000024366333293     
+   4.1999987142469690 
+EOF
+test_values.sh multi.tmp answer.tmp 1 "fitutil: linear + sinusoid + exponential" || exit 1
+
+#####
+#	CLEAN UP
+#####
+rm *.tmp
