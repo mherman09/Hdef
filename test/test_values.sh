af414caf58fc8d46a2b5d3f5ccac8c0716fdca77
mherman09 <matthew.w.herman@gmail.com>
2019-09-11 17:24:00 +0200

    Modify test_values.sh
    1. Add option to set zero value on command line.
    2. Correct absolute value of expected in test.

--- test/test_values.sh
+++ test/test_values.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
 function usage {
-    echo "Usage: $0 COMPUTED EXPECTED NFIELDS MESSAGE" 1>&2
+    echo "Usage: $0 COMPUTED EXPECTED NFIELDS MESSAGE [-zero THR]" 1>&2
     exit 1
 }
 
 # Check for files and number of fields
-if [ ! -f "$1" ]; then echo "Missing computed value file" 1>&2; usage; fi
-if [ ! -f "$2" ]; then echo "Missing expected value file" 1>&2; usage; fi
-if [ $# -lt 3 ]; then echo "Missing number of fields" 1>&2; usage; fi
+COMPUTED="$1"
+EXPECTED="$2"
+NFIELDS="$3"
+MESSAGE="$4"
+if [ ! -f "$COMPUTED" ]; then echo "Missing computed value file" 1>&2; usage; fi
+if [ ! -f "$EXPECTED" ]; then echo "Missing expected value file" 1>&2; usage; fi
+if [ "$NFIELDS" == "" ]; then echo "Missing number of fields" 1>&2; usage; fi
+if [ "$MESSAGE" == "" ]; then echo "Missing descriptive error message" 1>&2; usage; fi
+
+# Parse optional arguments
+THRESHOLD="1.0e-6"
+if [ $# -ge 5 ]
+then
+    shift;shift;shift;shift
+    while [ "$1" != "" ]
+    do
+        case $1 in
+            -zero) shift;THRESHOLD=$1;echo "$0: new zero level set to $THRESHOLD";;
+            *) echo "$0: no option \"$1\"" 1>&2;;
+        esac
+        shift
+    done
+fi
 
 # Compare computed and expected values
-THRESHOLD="1.0e-6"
-paste $1 $2 |\
+paste $COMPUTED $EXPECTED |\
     awk '{
-        for (i=1;i<='$3';i++) {
+        for (i=1;i<='$NFIELDS';i++) {
             computed = $i
-            expected = $('$3'+i)
+            expected = $('$NFIELDS'+i)
             diff = computed - expected
             if (diff<0) {
                 diff = -diff
             }
             if (expected<0) {
-                expected = -expected
+                expected_abs = -expected
+            } else {
+                expected_abs = expected
             }
-            if (expected<'"$THRESHOLD"'/10) {
+            if (expected_abs<'"$THRESHOLD"'/10) {
                 if (diff>'"$THRESHOLD"') {
                     print "FAIL"
                     printf("computed %14.6e\n"),computed > "/dev/stderr"
@@ -48,6 +69,6 @@ paste $1 $2 |\
 FAIL=`grep FAIL result.tmp`
 if [ "$FAIL" != "" ]
 then
-    echo Failed test $4 1>&2
+    echo Failed test $MESSAGE 1>&2
     exit 1
 fi
