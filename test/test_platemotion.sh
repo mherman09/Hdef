013cec5c2d857710d1a2b6a477047a333ba9b3cd
mherman09 <matthew.w.herman@gmail.com>
2019-09-11 17:22:43 +0200

    Modify test_platemotion.sh
    1. Update test script for platemotion to reflect changes in
       source code.

--- test/test_platemotion.sh
+++ test/test_platemotion.sh
@@ -2,12 +2,12 @@
 
 # MORVEL56
 echo -30 40 | ../bin/platemotion -plates NA/EU -model MORVEL56 -o morvel56.tmp
-echo -30.000000000000000 40.000000000000000 22.800363048707151 -2.0328133578957290 > answer.tmp
+echo   -30.000000000000000        40.000000000000000        22.771560776120207       -2.0222476466178430 > answer.tmp
 ./test_values.sh morvel56.tmp answer.tmp 4 "platemotion: MORVEL56 EU wrt NA, output file" || exit 1
 
 # MORVEL
 echo -75 -35 | ../bin/platemotion -plates SA/NZ -model MORVEL > morvel.tmp
-echo -75.000000000000000 -35.000000000000000 72.006366521106116 16.764646648370597 > answer.tmp
+echo -75.000000000000000       -35.000000000000000        72.008593269570923        16.689689796741547 > answer.tmp
 ./test_values.sh morvel.tmp answer.tmp 4 "platemotion: MORVEL SA wrt NZ, stdout" || exit 1
 
 # NUVEL1A
@@ -15,22 +15,22 @@ echo 115 -50 > loc.tmp
 echo 120 -50 >> loc.tmp
 ../bin/platemotion -f loc.tmp -plates AN/AU -model NUVEL1A > nuvel1a.tmp
 cat > answer.tmp << EOF
-   115.00000000000000       -50.000000000000000        22.825424943192935        68.402081486893053
-   120.00000000000000       -50.000000000000000        18.224424850629205        69.545096250232746
+   115.00000000000000       -50.000000000000000        22.881201729386667        68.378823029969183     
+   120.00000000000000       -50.000000000000000        18.268958516527089        69.521449139059513 
 EOF
 ./test_values.sh nuvel1a.tmp answer.tmp 4 "platemotion: NUVEL1A AU wrt AN, stdout" || exit 1
 
 # ITRF08
 ../bin/platemotion -f loc.tmp -plates AN/AU -model ITRF08 > itrf08.tmp
 cat > answer.tmp << EOF
-   115.00000000000000       -50.000000000000000        25.705288708213111        68.353999312708282     
-   120.00000000000000       -50.000000000000000        21.100660742486028        69.701949312051511     
+   115.00000000000000       -50.000000000000000        25.766441315239788        68.326351344076343     
+   120.00000000000000       -50.000000000000000        21.150858988809414        69.673756121784024  
 EOF
 ./test_values.sh itrf08.tmp answer.tmp 4 "platemotion: ITRF08 AU wrt AN, stdout" || exit 1
 
 # Custom pole
 echo -30 40 | ../bin/platemotion -pole 139.461/61.796/0.211 -o custom.tmp
-echo -30.000000000000000 40.000000000000000 22.875128456072254 -2.0387182521615239 > answer.tmp
+echo   -30.000000000000000        40.000000000000000        22.846411219358291       -2.0281377828526641 > answer.tmp 
 ./test_values.sh custom.tmp answer.tmp 4 "platemotion: Custom pole, file" || exit 1
 
 # Clean up
