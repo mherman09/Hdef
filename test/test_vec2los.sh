a7f37c077f70feaf31e2065bafef8063b74b885a
mherman09 <matthew.w.herman@gmail.com>
2020-04-23 10:30:21 +0200

    Find Hdef executables in test scripts
    Look for Hdef executables in PATH environment variable, the same
    directory as test script, or the relative directory ../bin.
    
    Set the TEST_BIN_DIR variable automatically to be the same
    location as the test script.
    
    Call trap to remove temporary files on exit.

--- test/test_vec2los.sh
+++ test/test_vec2los.sh
@@ -36,24 +36,27 @@ fi
 
 
 #####
-#	SET PATH TO TEST_VALUES SCRIPT
+#       SET PATH TO TEST_VALUES SCRIPT
 #####
 TEST_BIN_DIR=`echo $0 | xargs dirname`
 
 
 #####
-#	RUN TEST
+#       RUN TEST
 #####
+# Clean up tmp files
 trap "rm -f *.tmp" 0 1 2 3 8 9
 
-
+# Standard input to vec2los
 echo 4 5 6 1 2 3 > vec.tmp
 cat vec.tmp | $BIN_DIR/vec2los -a 45 -i 30 > los.tmp
 cat > answer.tmp << EOF
 4 5 6 0.33711730708738386
 EOF
-$TEST_BIN_DIR/test_values.sh los.tmp answer.tmp 4 "vec2los: standard in/out"
+$TEST_BIN_DIR/test_values.sh los.tmp answer.tmp 4 "vec2los: read input from stdin"  || \
+    { echo "$0: error in vec2los reading from standard input" 1>&2; exit 1; }
 
+# Read vec2los input from file
 echo 4 5 6 -1 -2 -3 >> vec.tmp
 echo 45 30 > look.tmp
 echo 25 20 >> look.tmp
@@ -62,4 +65,5 @@ cat > answer.tmp << EOF
 4 5 6 -1.0743723112683896
 EOF
 $BIN_DIR/vec2los -f vec.tmp -look look.tmp > los.tmp
-$TEST_BIN_DIR/test_values.sh los.tmp answer.tmp 4 "vec2los: read look orientations"
+$TEST_BIN_DIR/test_values.sh los.tmp answer.tmp 4 "vec2los: read input from file" || \
+    { echo "$0: error in vec2los reading input from file" 1>&2; exit 1; }
--- test/test_wraplos.sh
+++ test/test_wraplos.sh
@@ -44,20 +44,24 @@ TEST_BIN_DIR=`echo $0 | xargs dirname`
 #####
 #	RUN TEST
 #####
+# Clean up tmp files
 trap "rm -f *.tmp" 0 1 2 3 8 9
 
-
+# Standard input to wraplos
 echo 1 2 3 4 > los.tmp
 cat los.tmp | $BIN_DIR/wraplos -w 0.17 > phase.tmp
 cat > answer.tmp << EOF
    1.0000000000000000        2.0000000000000000        3.0000000000000000       0.36959913571640612
 EOF
-$TEST_BIN_DIR/test_values.sh phase.tmp answer.tmp 4 "wraplos: standard in/out"
+$TEST_BIN_DIR/test_values.sh phase.tmp answer.tmp 4 "wraplos: read input from stdin" || \
+    { echo "$0: error in wraplos reading from standard input" 1>&2; exit 1; }
 
+# Read wraplos input from file
 echo 5 6 7 8 >> los.tmp
 cat > answer.tmp << EOF
    1.0000000000000000        2.0000000000000000        3.0000000000000000       0.36959913571640612
    5.0000000000000000        6.0000000000000000        7.0000000000000000       0.73919827143281225
 EOF
 $BIN_DIR/wraplos -f los.tmp -w 0.17 > phase.tmp
-$TEST_BIN_DIR/test_values.sh phase.tmp answer.tmp 4 "wraplos: read input"
+$TEST_BIN_DIR/test_values.sh phase.tmp answer.tmp 4 "wraplos: read input from file" || \
+    { echo "$0: error in wraplos reading input from file" 1>&2; exit 1; }
