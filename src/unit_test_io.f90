ece80a2be8eff7f2fa07cf9d1319a9490b89f0ad
mherman09 <matthew.w.herman@gmail.com>
2020-05-31 13:57:15 +0200

    Add unit test for io module
    Test functions in the io module. Add this test to the Makefile
    template.

---
+++ src/unit_test_io.f90
@@ -0,0 +1,34 @@
+program main
+
+use test, only: test_value
+use io
+
+implicit none
+
+integer :: i
+
+call test_value(stdin,5,'stdin')
+call test_value(stdout,6,'stdout')
+call test_value(stderr,0,'stderr')
+
+open(unit=11,file='io_module_unit_test',status='unknown')
+write(11,*) 1
+write(11,*) 2
+write(11,*) 3
+write(11,*) 4
+close(11)
+
+if (fileExists('io_module_unit_test')) then
+    i = 1
+else
+    i = 0
+endif
+call test_value(i,1,'fileExists')
+
+i = line_count('io_module_unit_test')
+call test_value(i,4,'line_count')
+
+! subroutine progress_indicator()
+
+write(stdout,*) 'io_module unit test passed'
+end
